f526f5fce58e442a4f4143221251183f0fdae234
Emily McMullan <55964550+eemcmullan@users.noreply.github.com>
2021-06-30 13:20:20 -0400

    Added a Ginkgo starting test (#125)
    * added sample Ginkgo test for e2e tests
    
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Delete .DS_Store

---
+++ tests/e2e/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestAdd(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Add Nums Suite")
+}
+
+// to run test, run command 'ginkgo'
+var _ = Describe("Adding two nums", func() {
+	Context("When  3 is added to 6", func() {
+		It("Should return 9", func() {
+			result := add_nums(3, 6)
+			expected := 9
+			Expect(expected).To(Equal(result))
+		})
+	})
+})
