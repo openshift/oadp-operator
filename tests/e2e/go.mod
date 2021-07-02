43257fdc768f08fe2fd2594dec7830c2b776c4e7
Emily McMullan <eemcmullan92@gmail.com>
2021-07-02 16:49:40 -0400

    added functions to install velero

--- tests/e2e/go.mod
+++ tests/e2e/go.mod
@@ -1,8 +1,15 @@
-module add_nums
+module e2e-tests
 
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v0.0.0-20160705203006-01aeca54ebda // indirect
+	github.com/docker/spdystream v0.0.0-20160310174837-449fdfce4d96 // indirect
+	github.com/gophercloud/gophercloud v0.0.0-20190126172459-c818fa66e4c8 // indirect
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.13.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.21.2 // indirect
+	k8s.io/client-go v0.21.2 // indirect
+	k8s.io/klog v0.3.1 // indirect
 )
--- tests/e2e/main.go
+++ tests/e2e/main.go
@@ -1,12 +1,139 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
 
-func add_nums(a int, b int) int {
-	sum := a + b
-	return sum
-}
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const VeleroYAML = `apiVersion: konveyor.openshift.io/v1alpha1
+kind: Velero
+metadata:
+  name: example-velero
+spec:
+  olm_managed: false
+  default_velero_plugins:
+  - aws
+  - openshift
+  - csi
+  backup_storage_locations:
+  - name: default
+    provider: aws
+    object_storage:
+      bucket: myBucket
+      prefix: "velero"
+    config:
+      region: us-east-1
+      profile: "default"
+    credentials_secret_ref:
+      name: cloud-credentials
+      namespace: oadp-operator
+  volume_snapshot_locations:
+  - name: default
+    provider: aws
+    config:
+      region: us-west-2
+      profile: "default"
+  enable_restic: true
+  velero_feature_flags: EnableCSI`
 
 func main() {
-	fmt.Println(add_nums(3, 6))
+	installVelero()
+
+	// get Velero pod status
+	veleroStatus := getPodStatus()
+	fmt.Println(veleroStatus)
+}
+
+func decodeYaml() *unstructured.Unstructured {
+	// set new unstructured type for Velero CR
+	unstructVelero := &unstructured.Unstructured{}
+
+	// decode yaml into unstructured type
+	dec := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	_, _, err := dec.Decode([]byte(VeleroYAML), nil, unstructVelero)
+	if err != nil {
+		panic(err)
+	}
+	return unstructVelero
+}
+
+func createVeleroClient(res *unstructured.Unstructured, client dynamic.Interface) (dynamic.ResourceInterface, error) {
+
+	resourceClient := client.Resource(schema.GroupVersionResource{
+		Group:    "konveyor.openshift.io",
+		Version:  "v1alpha1",
+		Resource: "veleros",
+	})
+	namespaceResClient := resourceClient.Namespace("oadp-operator")
+
+	return namespaceResClient, nil
+}
+
+func createVeleroCR(res *unstructured.Unstructured, client dynamic.Interface) (unstructured.Unstructured, error) {
+	veleroClient, err := createVeleroClient(res, client)
+	if err != nil {
+		panic(err)
+	}
+	createdResource, err := veleroClient.Create(context.Background(), res, v1.CreateOptions{})
+	if apierrors.IsAlreadyExists(err) {
+		fmt.Println("Resource already exists")
+	} else if err != nil {
+		fmt.Println("Error creating resource")
+	} else {
+		fmt.Println("Velero resource successfully created")
+	}
+	return *createdResource, nil
+}
+
+func installVelero() {
+	// get cluster config
+	config, err := clientcmd.BuildConfigFromFlags("", "/Users/emilymcmullan/.agnosticd/ocp4-emcmulla-06232_kubeconfig")
+	if err != nil {
+		panic(err)
+	}
+	// create dynamic client
+	client, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	// get Velero unstruct type to create Velero CR
+	unstrVel := decodeYaml()
+	createVeleroCR(unstrVel, client)
+}
+
+func getPodStatus() string {
+	// get cluster config
+	config, err := clientcmd.BuildConfigFromFlags("", "/Users/emilymcmullan/.agnosticd/ocp4-emcmulla-06232_kubeconfig")
+	if err != nil {
+		panic(err)
+	}
+	// create client for pod
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	// select Velero pod with this label
+	veleroOptions := v1.ListOptions{
+		LabelSelector: "component=velero",
+	}
+	// get pods in the oadp-operator namespace
+	podList, err := clientset.CoreV1().Pods("oadp-operator").List(context.TODO(), veleroOptions)
+	if err != nil {
+		panic(err)
+	}
+	// get pod name and status with specified label selector
+	var status string
+	for _, podInfo := range (*podList).Items {
+		status = string(podInfo.Status.Phase)
+	}
+	return status
 }
