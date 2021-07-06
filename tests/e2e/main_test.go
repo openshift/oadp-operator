9037cf38526765c1edf924223ca75e0373f9b3e7
Emily McMullan <eemcmullan92@gmail.com>
2021-07-06 15:07:09 -0400

    test to verify Velero pod is running

--- tests/e2e/main_test.go
+++ tests/e2e/main_test.go
@@ -12,23 +12,15 @@ func TestVeleroE2E(t *testing.T) {
 	RunSpecs(t, "Velero E2E Suite")
 }
 
+var _ = BeforeSuite(func() {
+	installVelero()
+})
+
 var _ = Describe("Installing Velero", func() {
 	Context("When the default Velero CR is created", func() {
 		It("Should create a Velero pod in the cluster", func() {
-			//result := getPodStatus()
-			//expected := "Running"
-			//Expect(expected).To(Equal(result))
+			result := waitForPodRunning()
+			Expect(result).To(BeNil())
 		})
 	})
 })
-
-// to run test, run command 'ginkgo'
-// var _ = Describe("Adding two nums", func() {
-// 	Context("When  3 is added to 6", func() {
-// 		It("Should return 9", func() {
-// 			result := add_nums(3, 6)
-// 			expected := 9
-// 			Expect(expected).To(Equal(result))
-// 		})
-// 	})
-// })
