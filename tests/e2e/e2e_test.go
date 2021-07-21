3ea9dc638d36d66fe9da5971aab35503f23cc895
Dylan Murray <dymurray@redhat.com>
2021-07-21 10:32:56 -0400

    Test secret creation in vault (#138)

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -1,6 +1,8 @@
 package e2e
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -17,6 +19,10 @@ var _ = Describe("Adding two nums", func() {
 	Context("When  3 is added to 6", func() {
 		It("Should return 9", func() {
 			result := add_nums(3, 6)
+			file, err := os.Open("/var/run/oadp-credentials")
+			Expect(err).NotTo(HaveOccurred())
+			fmt.Println("FILE CONTENTS")
+			fmt.Print(file)
 			expected := 9
 			Expect(expected).To(Equal(result))
 		})
