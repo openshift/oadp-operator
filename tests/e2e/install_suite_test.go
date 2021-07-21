dcd6c744a72cc998f045e3e2f6d5ef8e2043676f
Emily McMullan <eemcmullan92@gmail.com>
2021-07-21 13:28:10 -0400

    added restic test

--- tests/e2e/install_suite_test.go
+++ tests/e2e/install_suite_test.go
@@ -5,13 +5,26 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = BeforeSuite(func() {
-	err := createOADPTestNamespace()
-	Expect(err).NotTo(HaveOccurred())
-	// Check that OADP operator is installed in test namespace
-})
+// var _ = BeforeSuite(func() {
+// 	err := installDefaultVelero()
+// 	Expect(err).ToNot(HaveOccurred())
 
-var _ = Describe("Creating Default Velero Custom Resource", func() {
+// 	//err := createOADPTestNamespace()
+// 	//Expect(err).NotTo(HaveOccurred())
+// 	// Check that OADP operator is installed in test namespace
+// })
+
+// var _ = AfterSuite(func() {
+// 	err := uninstallVelero()
+// 	Expect(err).ToNot(HaveOccurred())
+// })
+
+// Check Velero is deleted
+// TODO: Check test namespace is deleted
+// TODO: Check secret is deleted
+//})
+
+var _ = Describe("The default Velero custom resource", func() {
 	var _ = BeforeEach(func() {
 		err := installDefaultVelero()
 		Expect(err).NotTo(HaveOccurred())
@@ -21,18 +34,22 @@ var _ = Describe("Creating Default Velero Custom Resource", func() {
 		err := uninstallVelero()
 		Expect(err).NotTo(HaveOccurred())
 	})
-	Context("When the default valid Velero CR is created, but no credential secret is present", func() {
-		It("Should print an error to Velero CR status", func() {
-			err := waitForFailedVeleroCR()
-			Expect(err).NotTo(HaveOccurred())
-		})
-	})
+
+	// Context("When the default valid Velero CR is created, but no credential secret is present", func() {
+	// 	It("Should print an error to Velero CR status", func() {
+	// 		err := waitForFailedVeleroCR()
+	// 		Expect(err).NotTo(HaveOccurred())
+	// 	})
+	// })
+
 	Context("When the default valid Velero CR is created", func() {
-		It("Should create a Velero pod and Restic daemonset in the cluster", func() {
-			result := waitForVeleroPodRunning()
-			Expect(result).To(BeNil())
-			resticPodsResult := waitForResticPods()
-			Expect(resticPodsResult).To(BeNil())
+		It("Should create a Velero pod in the cluster", func() {
+			veleroResult := waitForVeleroPodRunning()
+			Expect(veleroResult).To(BeNil())
+		})
+		It("Should create a Restic daemonset in the cluster", func() {
+			resticResult := waitForResticPods()
+			Expect(resticResult).To(BeNil())
 		})
 	})
 })
