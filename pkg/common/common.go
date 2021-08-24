a1821abd0e68f06e42607a7afe63b879a8d58fd7
dymurray <dymurray@redhat.com>
2021-08-24 14:04:54 -0400

    Update codebase so e2e tests pass

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -6,3 +6,25 @@ const (
 	OADPOperator       = "oadp-operator"
 	OADPOperatorVelero = "oadp-operator-velero"
 )
+
+// Plugin names
+const (
+	VeleroPluginForAWS       = "velero-plugin-for-aws"
+	VeleroPluginForAzure     = "velero-plugin-for-microsoft-azure"
+	VeleroPluginForGCP       = "velero-plugin-for-gcp"
+	VeleroPluginForCSI       = "velero-plugin-for-csi"
+	VeleroPluginForOpenshift = "openshift-velero-plugin"
+)
+
+// Environment Vars keys
+const (
+	LDLibraryPathEnvKey            = "LD_LIBRARY_PATH"
+	VeleroNamespaceEnvKey          = "VELERO_NAMESPACE"
+	VeleroScratchDirEnvKey         = "VELERO_SCRATCH_DIR"
+	AWSSharedCredentialsFileEnvKey = "AWS_SHARED_CREDENTIALS_FILE"
+	AzureCredentialsFileEnvKey     = "AZURE_CREDENTIALS_FILE"
+	GCPCredentialsEnvKey           = "GOOGLE_APPLICATION_CREDENTIALS"
+	HTTPProxyEnvVar                = "HTTP_PROXY"
+	HTTPSProxyEnvVar               = "HTTPS_PROXY"
+	NoProxyEnvVar                  = "NO_PROXY"
+)
