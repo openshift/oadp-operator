8fb5560754d050d7dd83a56e71e28a95d817ea7f
Dylan Murray <dymurray@redhat.com>
2021-09-08 16:31:34 -0400

    Backup Restore test suite (#282)
    * Add backup test suite
    
    * Add backup functionality
    
    * Finish backup/restore suite

---
+++ tests/e2e/restore.go
@@ -0,0 +1,55 @@
+package e2e
+
+import (
+	"context"
+	velero "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func createRestoreFromBackup(ocClient client.Client, backupName, restoreName string) error {
+	restore := velero.Restore{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      restoreName,
+			Namespace: namespace,
+		},
+		Spec: velero.RestoreSpec{
+			BackupName: backupName,
+		},
+	}
+	err := ocClient.Create(context.Background(), &restore)
+	return err
+}
+
+func isRestoreDone(ocClient client.Client, name string) wait.ConditionFunc {
+	return func() (bool, error) {
+		restore := velero.Restore{}
+		err := ocClient.Get(context.Background(), client.ObjectKey{
+			Namespace: namespace,
+			Name:      name,
+		}, &restore)
+		if err != nil {
+			return false, err
+		}
+		if restore.Status.Phase != "" && restore.Status.Phase != velero.RestorePhaseNew && restore.Status.Phase != velero.RestorePhaseInProgress {
+			return true, nil
+		}
+		return false, nil
+	}
+}
+
+func isRestoreCompletedSuccessfully(ocClient client.Client, name string) (bool, error) {
+	restore := velero.Restore{}
+	err := ocClient.Get(context.Background(), client.ObjectKey{
+		Namespace: namespace,
+		Name:      name,
+	}, &restore)
+	if err != nil {
+		return false, err
+	}
+	if restore.Status.Phase == velero.RestorePhaseCompleted {
+		return true, nil
+	}
+	return false, nil
+}
