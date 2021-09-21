d1da0e1ad83f4406902f571be995493aad467edd
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2021-09-20 23:14:50 -0400

    What woudl CI do .go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+//What woudl CI do .go
 package main
 
 import (
