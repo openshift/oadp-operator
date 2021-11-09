59b9c0d3893478ece09af02830098bf42d50f3e9
Deepak Raj D S <drajds@redhat.com>
2021-11-08 20:41:53 -0500

    Removing debug statements

--- controllers/registry.go
+++ controllers/registry.go
@@ -547,7 +547,6 @@ func (r *VeleroReconciler) parseAWSSecret(secret corev1.Secret, secretKey string
 			}
 			cleanedLine := strings.ReplaceAll(line, " ", "")
 			parsedProfile := awsProfileRegex.ReplaceAllString(cleanedLine, "")
-			fmt.Println(parsedProfile, awsProfile, parsedProfile == awsProfile)
 			if parsedProfile == awsProfile {
 				// check for end of arr
 				if index+1 >= len(splitString) {
@@ -557,7 +556,6 @@ func (r *VeleroReconciler) parseAWSSecret(secret corev1.Secret, secretKey string
 					if profLine == "" {
 						continue
 					}
-					fmt.Println(profLine)
 					matchedAccessKey := awsAccessKeyRegex.MatchString(profLine)
 
 					if err != nil {
