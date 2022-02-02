5c8dc34bd3c49664aff8f2e9b92f449fd619548d
nitishSr <nsrivast@redhat.com>
2022-02-02 11:24:17 +0530

    Removed commented code

--- tests/e2e/lib/kube_helpers.go
+++ tests/e2e/lib/kube_helpers.go
@@ -166,17 +166,6 @@ func serverVersion() (*version.Info, error) {
 	return clientset.Discovery().ServerVersion()
 }
 
-// func ReadFile(path string) ([]byte, error) {
-// 	// pass in aws credentials by cli flag
-// 	// from cli:  -cloud=<"filepath">
-// 	// go run main.go -cloud="/Users/emilymcmullan/.aws/credentials"
-// 	// cloud := flag.String("cloud", "", "file path for aws credentials")
-// 	// flag.Parse()
-// 	// save passed in cred file as []byteq
-// 	file, err := ioutil.ReadFile(path)
-// 	return file, err
-// }
-
 func CreateCredentialsSecret(data []byte, namespace string, credSecretRef string) error {
 	clientset, err := setUpClient()
 	if err != nil {
