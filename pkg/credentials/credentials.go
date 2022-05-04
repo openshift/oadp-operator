7b49d25a9b7a370d85736f87c1c474d749c9d577
Tiger Kaovilai <tkaovila@redhat.com>
2022-05-03 23:58:48 -0400

    restic only mount secretVolume if NoDefaultBackupLocation is found.

--- pkg/credentials/credentials.go
+++ pkg/credentials/credentials.go
@@ -175,14 +175,12 @@ func AppendCloudProviderVolumes(dpa *oadpv1alpha1.DataProtectionApplication, ds 
 		// ok is boolean that will be true if `plugin` is a valid key in `PluginSpecificFields` map
 		// pattern from https://golang.org/doc/effective_go#maps
 		// this replaces the need to iterate through the `pluginSpecificFields` O(n) -> O(1)
-		if cloudProviderMap, ok := PluginSpecificFields[plugin]; ok {
-			if !cloudProviderMap.IsCloudProvider {
-				continue
-			}
+		if cloudProviderMap, ok := PluginSpecificFields[plugin]; ok &&
+			cloudProviderMap.IsCloudProvider &&
+			!dpa.Spec.Configuration.Velero.NoDefaultBackupLocation {
 
-			pluginNeedsCheck, foundInBSLorVSL := providerNeedsDefaultCreds[string(plugin)]
-
-			if !foundInBSLorVSL && !hasCloudStorage {
+			pluginNeedsCheck, foundProviderPlugin := providerNeedsDefaultCreds[string(plugin)]
+			if !foundProviderPlugin && !hasCloudStorage {
 				pluginNeedsCheck = true
 			}
 
