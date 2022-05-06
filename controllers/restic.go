c0dc1a68726ec19d1b46df03688c12df60394017
Tiger Kaovilai <tkaovila@redhat.com>
2022-05-06 09:43:40 -0400

    clean up restic config nil condition

--- controllers/restic.go
+++ controllers/restic.go
@@ -175,18 +175,17 @@ func (r *DPAReconciler) buildResticDaemonset(dpa *oadpv1alpha1.DataProtectionApp
 }
 
 func (r *DPAReconciler) customizeResticDaemonset(dpa *oadpv1alpha1.DataProtectionApplication, ds *appsv1.DaemonSet) (*appsv1.DaemonSet, error) {
-	// add custom pod labels
-	if dpa.Spec.Configuration.Restic != nil {
-		if dpa.Spec.Configuration.Restic.PodConfig != nil && dpa.Spec.Configuration.Restic.PodConfig.Labels != nil {
-			var err error
-			ds.Spec.Template.Labels, err = common.AppendUniqueLabels(ds.Spec.Template.Labels, dpa.Spec.Configuration.Restic.PodConfig.Labels)
-			if err != nil {
-				return nil, fmt.Errorf("restic daemonset template custom label: %s", err)
-			}
-		}
-	} else {
+	if dpa.Spec.Configuration.Restic == nil {
 		// if restic is not configured, therefore not enabled, return early.
 		return nil, nil
+	}
+	// add custom pod labels
+	if dpa.Spec.Configuration.Restic.PodConfig != nil && dpa.Spec.Configuration.Restic.PodConfig.Labels != nil {
+		var err error
+		ds.Spec.Template.Labels, err = common.AppendUniqueLabels(ds.Spec.Template.Labels, dpa.Spec.Configuration.Restic.PodConfig.Labels)
+		if err != nil {
+			return nil, fmt.Errorf("restic daemonset template custom label: %s", err)
+		}
 	}
 	// customize specs
 	ds.Spec.Selector = resticLabelSelector
