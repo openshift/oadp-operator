6d9e32cc359131c81f6d65684d1f61756553a631
Wes Hayutin <weshayutin@gmail.com>
2022-08-29 10:22:27 -0600

    Enable e2e tests to execute against downstream build
    Reusing the e2e tests across upstream and downstream builds
    will benefit the engineering teams by increasing the coverage
    across teams.  Additionally this should enable QE to execute
    the dev teams e2e tests against a build.
    
    * New Variable = "stream" ( ok name? )
      * allow cli -stream or env var OADP_STREAM to set value [up,down]
    * Only change required was the subscription label
    
    Tested and passed on internal build

--- tests/e2e/subscription_suite_test.go
+++ tests/e2e/subscription_suite_test.go
@@ -36,11 +36,12 @@ var _ = Describe("Subscription Config Suite Test", func() {
 	type SubscriptionConfigTestCase struct {
 		operators.SubscriptionConfig
 		failureExpected *bool
+		stream string
 	}
 	DescribeTable("Proxy test table",
 		func(testCase SubscriptionConfigTestCase) {
 			log.Printf("Getting Operator Subscription")
-			s, err := dpaCR.GetOperatorSubscription()
+			s, err := dpaCR.GetOperatorSubscription(stream)
 			Expect(err).To(BeNil())
 			log.Printf("Setting test case subscription config")
 			s.Spec.Config = &testCase.SubscriptionConfig
@@ -108,6 +109,7 @@ var _ = Describe("Subscription Config Suite Test", func() {
 					},
 				},
 			},
+			stream: stream,
 		}),
 		Entry("NO_PROXY set", SubscriptionConfigTestCase{
 			SubscriptionConfig: operators.SubscriptionConfig{
@@ -118,6 +120,7 @@ var _ = Describe("Subscription Config Suite Test", func() {
 					},
 				},
 			},
+			stream: stream,
 		}),
 		Entry("HTTPS_PROXY set", SubscriptionConfigTestCase{
 			SubscriptionConfig: operators.SubscriptionConfig{
@@ -130,6 +133,7 @@ var _ = Describe("Subscription Config Suite Test", func() {
 			},
 			// Failure is expected because localhost is not a valid https proxy and manager container will fail setup
 			failureExpected: pointer.Bool(true),
+			stream: stream,
 		}),
 		// Leave this as last entry to reset config
 		Entry("Config unset", SubscriptionConfigTestCase{
