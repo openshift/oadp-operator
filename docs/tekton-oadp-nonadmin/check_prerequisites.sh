16d066c64620dd60c91551f495c1efb4c40187f6
Wesley Hayutin <weshayutin@gmail.com>
2023-03-17 15:18:37 -0600

    fix up prereq script

--- docs/tekton-oadp-nonadmin/check_prerequisites.sh
+++ docs/tekton-oadp-nonadmin/check_prerequisites.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTALL_SAMPLE="${INSTALL_SAMPLE:-false}"
+INSTALLSAMPLE="${INSTALLSAMPLE:-false}"
 ############################################################
 # Help                                                     #
 ############################################################
@@ -9,7 +9,7 @@ Help()
    # Display Help
    echo "Check the prerequisites for the OADP non-admin demo"
    echo
-   echo "Syntax: scriptTemplate [-h|-p|-u|-d]"
+   echo "Check the prerequisites [-h|-i]"
    echo "options:"
    echo "h     Print this Help."
    echo "i     Install nginx-example"
@@ -24,13 +24,13 @@ Help()
 # Process the input options. Add options as needed.        #
 ############################################################
 # Get the options
-while getopts ":h:i:" option; do
+while getopts "h:i" option; do
    case $option in
       h) # display Help
          Help
          exit;;
-      i) # Enter a project name 
-	 INSTALL_SAMPLE=true;;
+      i) # Enter a demo user base name 
+	 INSTALLSAMPLE="true";;
      \?) # Invalid option
          echo "Error: Invalid option"
 	 Help
@@ -38,37 +38,46 @@ while getopts ":h:i:" option; do
    esac
 done
 
-
+printf "\n"
 printf "Checking if OADP is installed and configured....\n"
-orc=`oc get operator oadp-operator.openshift-adp > /dev/null && echo $?`
-drc=`oc get dpa -n openshift-adp > /dev/null && echo $?`
-dparc=`oc get dpa dpa-sample -n openshift-adp -o jsonpath='{.metadata.name}'` || true
-if [ $orc -eq 0 ] && [ $drc -eq 0 ] && [ $dparc == "dpa-sample" ]; then 
-  printf "OADP is installed and configured correctly\n" 
-elif [ $orc -ne 0 ]; then
-  printf "OADP is NOT installed, please install OADP\n"
-  printf "https://docs.openshift.com/container-platform/4.12/backup_and_restore/application_backup_and_restore/installing/about-installing-oadp.html\n"
-  exit 1
-elif [ $drc -ne 0 ]; then
-  printf "OADP is not configured with a DPA\n"
-  printf "Please configure a DPA named dpa-sample\n"
-  printf "https://github.com/openshift/oadp-operator/blob/master/docs/install_olm.md#create-the-dataprotectionapplication-custom-resource"
-  exit 1
-elif [ $dparc != "dpa-sample" ]; then
-  printf "At this time the DPA name must be configured to be dpa-sample"
-  exit 1
+orc=`oc get operator | grep -c oadp-operator.openshift-adp`
+if [ $orc -eq 1 ]; then
+   drc=`oc get dpa -n openshift-adp | grep -c dpa-sample`
+   dparc=`oc get dpa dpa-sample -n openshift-adp -o jsonpath='{.metadata.name}'` || true
+   if [ $drc -eq 1 ] && [ $dparc == "dpa-sample" ]; then 
+   printf "OADP is installed and configured correctly\n" 
+   elif [ $drc -ne 1 ]; then
+   printf "OADP is not configured with a DPA\n"
+   printf "Please configure a DPA named dpa-sample\n"
+   printf "https://github.com/openshift/oadp-operator/blob/master/docs/install_olm.md#create-the-dataprotectionapplication-custom-resource"
+   printf "\n"
+   exit 1
+   elif [ $dparc != "dpa-sample" ]; then
+   printf "At this time the DPA name must be configured to be dpa-sample"
+   exit 1
+   fi
+else 
+   printf "OADP is NOT installed, please install OADP\n"
+   printf "https://docs.openshift.com/container-platform/4.12/backup_and_restore/application_backup_and_restore/installing/about-installing-oadp.html\n"
+   printf "\n"
+   exit 1
 fi
+printf "\n\n"
 
 printf "Checking for a sample application nginx-example\n"
-nrc=`oc get namespace nginx-example > /dev/null && echo $?`
-drc=`oc get deployment -n nginx-example | grep "2/2" > /dev/null && echo $?`
+nrc=`oc get namespace | grep -c nginx-example`
+ndrc=`oc get deployment -n nginx-example | grep -c "2\/2"` || true
 
-if [ $nrc -eq 0 ] && [ $drc -eq 0 ]; then
-  printf "The nginx-example sample application is deployed and running\n"
-elif [ $INSTALL_SAMPLE == "true" ]; then
+if [ $nrc -eq 1 ]; then
+  printf "The nginx-example namespace found\n"
+   if [ $ndrc -eq 1 ]; then
+      printf "The nginx-example deployment was found\n"
+   fi
+fi
+if [ $INSTALLSAMPLE == "true" ] && [ $nrc -eq 0 ]; then
   printf "Installing the nginx sample application\n"
   oc create -f ../examples/manifests/nginx/nginx-deployment.yaml 
-elif [ $INSTALL_SAMPLE == "false" ]; then
+elif [ $INSTALLSAMPLE == "false" ] && [ $nrc -eq 0 ]; then
   printf "Please set the flag to install the sample application or\n"
   printf "alternatively follow the instructions:\n"
   printf "https://github.com/openshift/oadp-operator/blob/master/docs/examples/stateless.md\n"
