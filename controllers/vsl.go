8fe5ca6baa19a2f29aa3c926b0a972aa3cd8fac2
Prasad Joshi <prajoshi@redhat.com>
2023-05-18 14:09:55 +0530

    OADP-1927: Fix VSL secret validation
    Signed-off-by: Prasad Joshi <prajoshi@redhat.com>

--- controllers/vsl.go
+++ controllers/vsl.go
@@ -57,22 +57,46 @@ func (r *DPAReconciler) LabelVSLSecrets(log logr.Logger) (bool, error) {
 		provider := strings.TrimPrefix(vsl.Velero.Provider, veleroIOPrefix)
 		switch provider {
 		case "aws":
-			secretName := credentials.PluginSpecificFields[oadpv1alpha1.DefaultPluginAWS].SecretName
-			_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
-			if err != nil {
-				return false, err
+			if vsl.Velero.Credential != nil {
+				secretName := vsl.Velero.Credential.Name
+				_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
+				if err != nil {
+					return false, err
+				}
+			} else {
+				secretName := credentials.PluginSpecificFields[oadpv1alpha1.DefaultPluginAWS].SecretName
+				_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
+				if err != nil {
+					return false, err
+				}
 			}
 		case "gcp":
-			secretName := credentials.PluginSpecificFields[oadpv1alpha1.DefaultPluginGCP].SecretName
-			_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
-			if err != nil {
-				return false, err
+			if vsl.Velero.Credential != nil {
+				secretName := vsl.Velero.Credential.Name
+				_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
+				if err != nil {
+					return false, err
+				}
+			} else {
+				secretName := credentials.PluginSpecificFields[oadpv1alpha1.DefaultPluginGCP].SecretName
+				_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
+				if err != nil {
+					return false, err
+				}
 			}
 		case "azure":
-			secretName := credentials.PluginSpecificFields[oadpv1alpha1.DefaultPluginMicrosoftAzure].SecretName
-			_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
-			if err != nil {
-				return false, err
+			if vsl.Velero.Credential != nil {
+				secretName := vsl.Velero.Credential.Name
+				_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
+				if err != nil {
+					return false, err
+				}
+			} else {
+				secretName := credentials.PluginSpecificFields[oadpv1alpha1.DefaultPluginMicrosoftAzure].SecretName
+				_, err := r.UpdateCredentialsSecretLabels(secretName, dpa.Namespace, dpa.Name)
+				if err != nil {
+					return false, err
+				}
 			}
 		}
 
