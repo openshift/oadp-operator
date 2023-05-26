c707a22408d360e40b9334b63a86b46ac11e397f
Wesley Hayutin <weshayutin@gmail.com>
2023-05-26 16:10:03 -0600

    put in restore checks

--- docs/tekton-oadp-nonadmin/oadp-tekton-container/check_backup_namespace.sh
+++ docs/tekton-oadp-nonadmin/oadp-tekton-container/check_backup_namespace.sh
@@ -7,5 +7,5 @@ if grep -q "$1" <<< "$2"; then
   echo "Found project $1 in the allowed list of projects for the user"
 else
   echo "The project $1 is not allowed for this user"
-  exit1
+  exit 1
 fi
