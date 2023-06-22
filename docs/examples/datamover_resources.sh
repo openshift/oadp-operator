8d38acccad46adcc8c785c33fa4ee5bd8be314dc
Wesley Hayutin <weshayutin@gmail.com>
2023-06-22 07:33:41 -0600

    fix count errors and formatting in datamover watch script

--- docs/examples/datamover_resources.sh
+++ docs/examples/datamover_resources.sh
@@ -51,10 +51,10 @@ fi
 function backup_summary () {
     echo -e "Get Backups:\n"
     oc -n openshift-adp exec deployment/velero -c velero -it -- ./velero get backup ;
-    echo -e "\nTotal Snapshots: " `oc get volumesnapshot -A | wc -l` ; 
-    echo "Total OADP Snapshots: " `oc get volumesnapshot -nopenshift-adp | wc -l` ; 
-    echo "Total SnapshotContents: " `oc get volumesnapshotcontents -A | wc -l` ; 
-    echo -e "\nTotal VSB: " `oc get vsb -A | wc -l` ; 
+    echo -e "\nTotal Snapshots: " `oc get volumesnapshot -A | sed 1d | wc -l` ; 
+    echo "Total OADP Snapshots: " `oc get volumesnapshot -n openshift-adp | sed 1d | wc -l` ; 
+    echo "Total SnapshotContents: " `oc get volumesnapshotcontents -A | sed 1d | wc -l` ; 
+    echo -e "\nTotal VSB: " `oc get vsb -A | sed 1d | wc -l` ; 
     echo "Completed: " `oc get vsb -A | grep -c Completed` ; 
     echo "InProgress: " `oc get vsb -A | grep -c InProgress` ; 
     echo "SnapshotBackupDone: " `oc get vsb -A | grep -c SnapshotBackupDone` ; 
@@ -62,13 +62,13 @@ function backup_summary () {
     echo "Completed: " `oc get vsb -A -oyaml | grep batching | grep -c Completed` ; 
     echo "Processing: " `oc get vsb -A -oyaml | grep batching | grep -c Processing` ; 
     echo "Queued: " `oc get vsb -A -oyaml | grep batching | grep -c Queued` ; 
-    echo -e "\nTotal ReplicationSources: " `oc get replicationsources -A | wc -l` ; echo -e "\n\n" ;
+    echo -e "\nTotal ReplicationSources: " `oc get replicationsources -A | sed 1d | wc -l` ;
 }
 
 function restore_summary () {
     echo -e "Get Restores:\n"
     oc -n openshift-adp exec deployment/velero -c velero -it -- ./velero get restore ; 
-    echo -e "\nTotal VSR: " `oc get vsr -A | wc -l` ; 
+    echo -e "\nTotal VSR: " `oc get vsr -A | sed 1d | wc -l` ; 
     echo "Completed: " `oc get vsr -A | grep -c Completed` ; 
     echo "InProgress: " `oc get vsr -A | grep -c InProgress` ; 
     echo "SnapshotRestoreDone: " `oc get vsr -A | grep -c SnapshotRestoreDone` ; 
@@ -76,25 +76,25 @@ function restore_summary () {
     echo "Completed: " `oc get vsr -A -oyaml | grep batching | grep -c Completed` ; 
     echo "Processing: " `oc get vsr -A -oyaml | grep batching | grep -c Processing` ; 
     echo "Queued: " `oc get vsr -A -oyaml | grep batching | grep -c Queued` ; 
-    echo -e "\nTotal ReplicationDestinations: " `oc get replicationdestinations -A | wc -l` ;
+    echo -e "\nTotal ReplicationDestinations: " `oc get replicationdestinations -A | sed 1d | wc -l` ;
 }
 
 function vsc_details () {
-    echo -e "\n***** VOLUME SNAPSHOT CONTENTS ******\n"
+    echo -e "\n***** VOLUME SNAPSHOT CONTENTS ******"
     for i in $(oc get vsc -A -o=custom-columns=:.metadata.name); do
         oc get vsc $i -o jsonpath='{"Name: "}{@.metadata.name}{" ReadyToUse: "}{@.status.readyToUse}{" creationTime: " }{@.metadata.creationTimestamp}' | column -t
     done
 }
 
 function replicationsources_details () {
-    echo -e "\n***** REPLICATION SOURCE ******\n"
+    echo -e "\n***** REPLICATION SOURCE ******"
     for i in $(oc -n openshift-adp get replicationsources -A -o=custom-columns=:.metadata.name); do
         oc -n openshift-adp get replicationsources $i -o jsonpath='{"Name: "}{@.metadata.name}{" SyncDuration: "}{@.status.lastSyncDuration}{" sourcePVC: " }{@.spec.sourcePVC}' | column -t
     done
 }
 
 function replicationdestination_details () {
-    echo -e "\n***** REPLICATION DESTINATION ******\n"
+    echo -e "\n***** REPLICATION DESTINATION ******"
     for i in $(oc -n openshift-adp get replicationdestination -A -o=custom-columns=:.metadata.name); do
         oc -n openshift-adp get replicationdestination $i -o jsonpath='{"Name: "}{@.metadata.name}{" SyncDuration: "}{@.status.lastSyncDuration}{" sourcePVC: " }{@.spec.sourcePVC}' | column -t
     done
