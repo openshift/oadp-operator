d148253923f990997e58e994ea12633e12f35bee
Mateus Oliveira <msouzaol@redhat.com>
2023-09-28 11:42:21 -0300

    fixup! fix: Remove 1.1 CRD
    Signed-off-by: Mateus Oliveira <msouzaol@redhat.com>

--- controllers/restic.go
+++ controllers/restic.go
@@ -24,10 +24,6 @@ import (
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-
-	appsV1 "k8s.io/api/apps/v1"
-	apiErrors "k8s.io/apimachinery/pkg/api/errors"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -71,9 +67,9 @@ func (r *DPAReconciler) ReconcileResticDaemonset(log logr.Logger) (bool, error) 
 	// When updating from OADP version 1.1.x to 1.2.x, delete
 	// daemonset.apps/restic (it was renamed to daemonset.apps/node-agent)
 	// resticrepositories.velero.io CRD (it was renamed to backuprepositories.velero.io)
-	if err := r.Delete(r.Context, &appsV1.DaemonSet{ObjectMeta: metaV1.ObjectMeta{
+	if err := r.Delete(r.Context, &appsv1.DaemonSet{ObjectMeta: metav1.ObjectMeta{
 		Name: "restic", Namespace: r.NamespacedName.Namespace,
-	}}); err != nil && !apiErrors.IsNotFound(err) {
+	}}); err != nil && !errors.IsNotFound(err) {
 		return false, err
 	}
 	resticRepositoriesResource := &unstructured.Unstructured{}
@@ -83,7 +79,7 @@ func (r *DPAReconciler) ReconcileResticDaemonset(log logr.Logger) (bool, error) 
 		Kind:    "CustomResourceDefinition",
 	})
 	resticRepositoriesResource.SetName("resticrepositories.velero.io")
-	if err := r.Delete(r.Context, resticRepositoriesResource); err != nil && !apiErrors.IsNotFound(err) {
+	if err := r.Delete(r.Context, resticRepositoriesResource); err != nil && !errors.IsNotFound(err) {
 		return false, err
 	}
 
