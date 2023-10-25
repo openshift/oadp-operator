ab5d5b040b33f6b45551a899c37fb02af7025196
Tiger Kaovilai <tkaovila@redhat.com>
2023-10-25 12:43:38 -0400

    oadp-1.2: e2e: Add restore warning and in-cluster version to errorIgnorePatterns
    from https://github.com/openshift/oadp-operator/pull/1197
    to go alongside https://github.com/openshift/velero/pull/301

--- tests/e2e/lib/velero_helpers.go
+++ tests/e2e/lib/velero_helpers.go
@@ -168,6 +168,8 @@ var errorIgnorePatterns = []string{
 	// Ignore managed fields errors per https://github.com/vmware-tanzu/velero/pull/6110 and avoid e2e failure.
 	// https://prow.ci.openshift.org/view/gs/origin-ci-test/pr-logs/pull/openshift_oadp-operator/1126/pull-ci-openshift-oadp-operator-master-4.10-operator-e2e-aws/1690109468546699264#1:build-log.txt%3A686
 	"level=error msg=\"error patch for managed fields ",
+	"Unable to retrieve in-cluster version",
+	"restore warning",
 }
 
 func recoverFromPanicLogs(veleroNamespace string, panicReason interface{}, panicFrom string) string {
