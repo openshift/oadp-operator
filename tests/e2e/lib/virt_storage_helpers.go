d9b03979af64c4e799b21df3557b2eb7d43c5083
Matthew Arnold <marnold@redhat.com>
2024-04-12 11:28:08 -0400

    Remove storage helpers and no-template code path.
    Signed-off-by: Matthew Arnold <marnold@redhat.com>

--- tests/e2e/lib/virt_storage_helpers.go
+++ tests/e2e/lib/virt_storage_helpers.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -25,59 +24,6 @@ var dataSourceGVK = schema.GroupVersionResource{
 	Group:    "cdi.kubevirt.io",
 	Resource: "datasources",
 	Version:  "v1beta1",
-}
-
-func (v *VirtOperator) deletePvc(namespace, name string) error {
-	return v.Clientset.CoreV1().PersistentVolumeClaims(namespace).Delete(context.Background(), name, metav1.DeleteOptions{})
-}
-
-func (v *VirtOperator) detachPvc(namespace, name string) error {
-	pvc, err := v.Clientset.CoreV1().PersistentVolumeClaims(namespace).Get(context.Background(), name, metav1.GetOptions{})
-	if err != nil {
-		return err
-	}
-	if pvc == nil {
-		return fmt.Errorf("PVC %s/%s does not exist", namespace, name)
-	}
-
-	owners := make([]metav1.OwnerReference, 0)
-	for _, owner := range pvc.OwnerReferences {
-		if owner.Kind == "DataVolume" {
-			continue
-		}
-		owners = append(owners, owner)
-	}
-
-	pvc.OwnerReferences = owners
-	_, err = v.Clientset.CoreV1().PersistentVolumeClaims(namespace).Update(context.Background(), pvc, metav1.UpdateOptions{})
-	return err
-}
-
-func (v *VirtOperator) checkPvcExists(namespace, name string) bool {
-	pvc, err := v.Clientset.CoreV1().PersistentVolumeClaims(namespace).Get(context.Background(), name, metav1.GetOptions{})
-	if err != nil {
-		return false
-	}
-	if pvc == nil {
-		return false
-	}
-	return true
-}
-
-// Check if this PVC is still owned by a DataVolume.
-func (v *VirtOperator) checkPvcAttached(namespace, name string) bool {
-	pvc, err := v.Clientset.CoreV1().PersistentVolumeClaims(namespace).Get(context.Background(), name, metav1.GetOptions{})
-	if err != nil || pvc == nil {
-		return false
-	}
-
-	for _, owner := range pvc.OwnerReferences {
-		if owner.Kind == "DataVolume" {
-			return true
-		}
-	}
-
-	return false
 }
 
 func (v *VirtOperator) getDataVolume(namespace, name string) (*unstructured.Unstructured, error) {
@@ -118,26 +64,6 @@ func (v *VirtOperator) checkDataVolumeReady(namespace, name string) bool {
 	}
 	log.Printf("Phase of DataVolume %s/%s: %s", namespace, name, phase)
 	return phase == "Succeeded"
-}
-
-func (v *VirtOperator) getDataVolumeSize(namespace, name string) (string, error) {
-	unstructuredDataVolume, err := v.getDataVolume(namespace, name)
-	if err != nil {
-		log.Printf("Error getting DataVolume %s/%s: %v", namespace, name, err)
-		return "", err
-	}
-	if unstructuredDataVolume == nil {
-		return "", err
-	}
-	size, ok, err := unstructured.NestedString(unstructuredDataVolume.UnstructuredContent(), "spec", "pvc", "resources", "requests", "storage")
-	if err != nil {
-		log.Printf("Error getting size from DataVolume: %v", err)
-		return "", err
-	}
-	if !ok {
-		return "", err
-	}
-	return size, nil
 }
 
 // Create a DataVolume, accepting an unstructured source specification.
@@ -195,17 +121,6 @@ func (v *VirtOperator) createDataVolumeFromUrl(namespace, name, url, size string
 		},
 	}
 	return v.createDataVolumeFromSource(namespace, name, size, urlSource)
-}
-
-// Create a DataVolume as a clone of an existing PVC.
-func (v *VirtOperator) createDataVolumeFromPvc(sourceNamespace, sourceName, cloneNamespace, cloneName, size string) error {
-	pvcSource := map[string]interface{}{
-		"pvc": map[string]interface{}{
-			"name":      sourceName,
-			"namespace": sourceNamespace,
-		},
-	}
-	return v.createDataVolumeFromSource(cloneNamespace, cloneName, size, pvcSource)
 }
 
 // Create a DataVolume and wait for it to be ready.
@@ -291,105 +206,4 @@ func (v *VirtOperator) EnsureDataSourceFromPvc(pvcNamespace, pvcName, dsNamespac
 	}
 
 	return nil
-}
-
-// Remove owner reference from a PVC, so DV removal keeps the PVC.
-func (v *VirtOperator) DetachPvc(namespace, name string, timeout time.Duration) error {
-	// Retry if there are API server conflicts ("the object has been modified")
-	timeTaken := 0 * time.Second
-	err := wait.PollImmediate(5*time.Second, timeout, func() (bool, error) {
-		timeTaken += 5
-		innerErr := v.detachPvc(namespace, name)
-		if innerErr != nil {
-			if apierrors.IsConflict(innerErr) {
-				log.Printf("PVC modification conflict, trying again...")
-				return false, nil // Conflict: try again
-			}
-			return false, innerErr // Anything else: give up
-		}
-		return innerErr == nil, nil
-	})
-
-	if err != nil {
-		return fmt.Errorf("timed out waiting to remove DataVolume as owner of PVC %s/%s", namespace, name)
-	}
-
-	timeout = timeout - timeTaken
-	err = wait.PollImmediate(5*time.Second, timeout, func() (bool, error) {
-		return !v.checkPvcAttached(namespace, name), nil
-	})
-
-	return err
-}
-
-func (v *VirtOperator) RemovePvc(namespace, name string, timeout time.Duration) error {
-	err := v.deletePvc(namespace, name)
-	if err != nil {
-		if apierrors.IsNotFound(err) {
-			log.Printf("PVC %s/%s already removed", namespace, name)
-		} else {
-			return err
-		}
-	}
-
-	err = wait.PollImmediate(5*time.Second, timeout, func() (bool, error) {
-		return !v.checkPvcExists(namespace, name), nil
-	})
-	if err != nil {
-		return fmt.Errorf("timed out waiting for PVC %s/%s to be deleted: %w", namespace, name, err)
-	}
-
-	log.Printf("Removed PVC %s/%s", namespace, name)
-
-	return nil
-}
-
-// Clone a DataVolume and wait for the copy to be ready.
-func (v *VirtOperator) CloneDisk(sourceNamespace, sourceName, cloneNamespace, cloneName string, timeout time.Duration) error {
-	log.Printf("Cloning %s/%s to %s/%s...", sourceNamespace, sourceName, cloneNamespace, cloneName)
-	if !v.checkDataVolumeExists(sourceNamespace, sourceName) {
-		return fmt.Errorf("source disk does not exist")
-	}
-
-	size, err := v.getDataVolumeSize(sourceNamespace, sourceName)
-	if err != nil {
-		return fmt.Errorf("failed to get disk size for clone: %w", err)
-	}
-
-	if err := v.createDataVolumeFromPvc(sourceNamespace, sourceName, cloneNamespace, cloneName, size); err != nil {
-		return fmt.Errorf("failed to clone disk: %w", err)
-	}
-
-	err = wait.PollImmediate(5*time.Second, timeout, func() (bool, error) {
-		return v.checkDataVolumeReady(cloneNamespace, cloneName), nil
-	})
-	if err != nil {
-		return fmt.Errorf("timed out waiting to clone DataVolume %s/%s to %s/%s: %w", sourceNamespace, sourceName, cloneNamespace, cloneName, err)
-	}
-
-	return nil
-}
-
-// Create a DataVolume from a sample YAML template. The namespace argument
-// should match a subdirectory under sample-applications/virtual-machines, and
-// the name argument should match a .yaml file in that directory, for example:
-//
-//	sample-applications/virtual-machines/example-vm-test/example-vm-test-disk.yaml
-//
-// This file must specify a DataVolume with the following annotations set:
-//
-//	cdi.kubevirt.io/storage.bind.immediate.requested: ""
-//	cdi.kubevirt.io/storage.deleteAfterCompletion: "false"
-//
-// This function will then wait for that DataVolume to be marked "Succeeded".
-func (v *VirtOperator) CreateDiskFromYaml(namespace, name string, timeout time.Duration) error {
-	if err := InstallApplication(v.Client, filepath.Join("sample-applications", "virtual-machines", namespace, name+".yaml")); err != nil {
-		return fmt.Errorf("failed to create DataVolume %s/%s: %w", namespace, name, err)
-	}
-
-	err := wait.PollImmediate(5*time.Second, timeout, func() (bool, error) {
-		return v.checkDataVolumeReady(namespace, name), nil
-	})
-
-	return err
 }
--- tests/e2e/virt_backup_restore_suite_test.go
+++ tests/e2e/virt_backup_restore_suite_test.go
@@ -48,59 +48,26 @@ type VmBackupRestoreCase struct {
 func runVmBackupAndRestore(brCase VmBackupRestoreCase, expectedErr error, updateLastBRcase func(brCase VmBackupRestoreCase), updateLastInstallTime func(), v *lib.VirtOperator) {
 	updateLastBRcase(brCase)
 
-	diskName := brCase.Name + "-disk"
-	vmName := brCase.Name + "-vm"
-
 	// Create DPA
 	backupName, restoreName := prepareBackupAndRestore(brCase.BackupRestoreCase, func() {})
 
 	err := lib.CreateNamespace(v.Clientset, brCase.Namespace)
 	gomega.Expect(err).To(gomega.BeNil())
 
-	if brCase.Template == "" { // No template: CirrOS test
-		// Create a standalone VM disk. This defaults to cloning the CirrOS image.
-		// This uses a DataVolume so import and clone progress can be monitored.
-		// Behavioral notes: CDI will garbage collect DataVolumes if they are not
-		// attached to anything, so this disk needs to include the annotation
-		// storage.deleteAfterCompletion in order to keep it around long enough to
-		// attach to a VM. CDI also waits until the DataVolume is attached to
-		// something before running whatever import process it has been asked to
-		// run, so this disk also needs the storage.bind.immediate.requested
-		// annotation to get it to start the cloning process.
-		err = v.CreateDiskFromYaml(brCase.Namespace, diskName, 5*time.Minute)
-		gomega.Expect(err).To(gomega.BeNil())
-
-		err = v.CreateVm(brCase.Namespace, vmName, 5*time.Minute)
-		gomega.Expect(err).To(gomega.BeNil())
-
-		// Remove the Data Volume, but keep the PVC attached to the VM. The
-		// DataVolume must be removed before backing up, because otherwise the
-		// restore process might try to follow the instructions in the spec. For
-		// example, a DataVolume that lists a cloned PVC will get restored in the
-		// same state, and attempt to clone the PVC again after restore. The
-		// DataVolume also needs to be detached from the VM, so that the VM restore
-		// does not try to use the DataVolume that was deleted.
-		err = v.DetachPvc(brCase.Namespace, diskName, 2*time.Minute)
-		gomega.Expect(err).To(gomega.BeNil())
-		err = v.RemoveDataVolume(brCase.Namespace, diskName, 2*time.Minute)
-		gomega.Expect(err).To(gomega.BeNil())
-
-	} else { // Fedora test
-		err = lib.InstallApplication(v.Client, brCase.Template)
-		if err != nil {
-			fmt.Printf("Failed to install VM template %s: %v", brCase.Template, err)
-		}
-		gomega.Expect(err).To(gomega.BeNil())
-
-		// Wait for VM to start, then give some time for cloud-init to run.
-		// Afterward, run through the standard application verification to make sure
-		// the application itself is working correctly.
-		err = wait.PollImmediate(10*time.Second, 10*time.Minute, func() (bool, error) {
-			status, err := v.GetVmStatus(brCase.Namespace, brCase.Name)
-			return status == "Running", err
-		})
-		gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	err = lib.InstallApplication(v.Client, brCase.Template)
+	if err != nil {
+		fmt.Printf("Failed to install VM template %s: %v", brCase.Template, err)
 	}
+	gomega.Expect(err).To(gomega.BeNil())
+
+	// Wait for VM to start, then give some time for cloud-init to run.
+	// Afterward, run through the standard application verification to make sure
+	// the application itself is working correctly.
+	err = wait.PollImmediate(10*time.Second, 10*time.Minute, func() (bool, error) {
+		status, err := v.GetVmStatus(brCase.Namespace, brCase.Name)
+		return status == "Running", err
+	})
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
 
 	// TODO: find a better way to check for clout-init completion
 	if brCase.InitDelay > 0*time.Second {
@@ -112,15 +79,8 @@ func runVmBackupAndRestore(brCase VmBackupRestoreCase, expectedErr error, update
 	nsRequiresResticDCWorkaround := runBackup(brCase.BackupRestoreCase, backupName)
 
 	// Delete everything in test namespace
-	if brCase.Template == "" { // No template: CirrOS test
-		err = v.RemoveVm(brCase.Namespace, vmName, 2*time.Minute)
-		gomega.Expect(err).To(gomega.BeNil())
-		err = v.RemovePvc(brCase.Namespace, diskName, 2*time.Minute)
-		gomega.Expect(err).To(gomega.BeNil())
-	} else { // Fedora test
-		err = v.RemoveVm(brCase.Namespace, brCase.Name, 2*time.Minute)
-		gomega.Expect(err).To(gomega.BeNil())
-	}
+	err = v.RemoveVm(brCase.Namespace, brCase.Name, 2*time.Minute)
+	gomega.Expect(err).To(gomega.BeNil())
 	err = lib.DeleteNamespace(v.Clientset, brCase.Namespace)
 	gomega.Expect(err).To(gomega.BeNil())
 	gomega.Eventually(lib.IsNamespaceDeleted(kubernetesClientForSuiteRun, brCase.Namespace), timeoutMultiplier*time.Minute*5, time.Second*5).Should(gomega.BeTrue())
