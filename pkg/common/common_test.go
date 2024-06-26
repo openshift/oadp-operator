544ccae3daa7de308688bb4ea6538982270b38e7
Michal Pryc <mpryc@redhat.com>
2024-06-26 09:32:43 +0200

    Unsupported args function to include multiple subCommands
    Addition to allow multiple sub commands to be used that
    is required for the node-agent that runs as:
    
     $ velero node-agent server [args]
    
    This change also modifies the args to be alphabetially
    sorted, so the result from the function is consistent across
    it's runs.
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- pkg/common/common_test.go
+++ pkg/common/common_test.go
@@ -2,7 +2,6 @@ package common
 
 import (
 	"reflect"
-	"sort"
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
@@ -205,13 +204,13 @@ func TestGetImagePullPolicy(t *testing.T) {
 func TestGenerateCliArgsFromConfigMap(t *testing.T) {
 	tests := []struct {
 		name          string
-		cliSubCommand string
+		cliSubCommand []string
 		configMap     *corev1.ConfigMap
 		expectedArgs  []string
 	}{
 		{
 			name:          "Boolean argument variations",
-			cliSubCommand: "server",
+			cliSubCommand: []string{"server"},
 			configMap: &corev1.ConfigMap{
 				Data: map[string]string{
 					"--default-snapshot-move":   "true",
@@ -222,15 +221,15 @@ func TestGenerateCliArgsFromConfigMap(t *testing.T) {
 			},
 			expectedArgs: []string{
 				"server",
-				"--default-snapshot-move=true",
 				"--another-key-mix-letters=true",
+				"--default-snapshot-move=true",
 				"--key-no-prefix=false",
 				"--string-not-bool='False'",
 			},
 		},
 		{
 			name:          "All arguments with spaces, some without sigle quotes",
-			cliSubCommand: "server",
+			cliSubCommand: []string{"server"},
 			configMap: &corev1.ConfigMap{
 				Data: map[string]string{
 					"default-volume-snapshot-locations": "aws:backups-primary, azure:backups-secondary",
@@ -245,7 +244,7 @@ func TestGenerateCliArgsFromConfigMap(t *testing.T) {
 		},
 		{
 			name:          "Preserve single and double '-' as key prefix",
-			cliSubCommand: "server",
+			cliSubCommand: []string{"server"},
 			configMap: &corev1.ConfigMap{
 				Data: map[string]string{
 					"-default-volume-snapshot-locations": "aws:backups-primary, azure:backups-secondary",
@@ -255,14 +254,14 @@ func TestGenerateCliArgsFromConfigMap(t *testing.T) {
 			},
 			expectedArgs: []string{
 				"server",
-				"-default-volume-snapshot-locations=aws:backups-primary, azure:backups-secondary",
 				"--log-level=debug",
+				"-default-volume-snapshot-locations=aws:backups-primary, azure:backups-secondary",
 				"-string-bool=false",
 			},
 		},
 		{
 			name:          "Non-Boolean argument with space",
-			cliSubCommand: "server",
+			cliSubCommand: []string{"server"},
 			configMap: &corev1.ConfigMap{
 				Data: map[string]string{
 					"default-snapshot-move-data": " true",
@@ -275,7 +274,7 @@ func TestGenerateCliArgsFromConfigMap(t *testing.T) {
 		},
 		{
 			name:          "Mixed arguments",
-			cliSubCommand: "server",
+			cliSubCommand: []string{"server"},
 			configMap: &corev1.ConfigMap{
 				Data: map[string]string{
 					"--default-volume-snapshot-locations": "aws:backups-primary,azure:backups-secondary",
@@ -285,14 +284,14 @@ func TestGenerateCliArgsFromConfigMap(t *testing.T) {
 			},
 			expectedArgs: []string{
 				"server",
+				"--default-snapshot-move-data=true",
 				"--default-volume-snapshot-locations=aws:backups-primary,azure:backups-secondary",
 				"--log-level=debug",
-				"--default-snapshot-move-data=true",
 			},
 		},
 		{
 			name:          "Empty ConfigMap",
-			cliSubCommand: "server",
+			cliSubCommand: []string{"server"},
 			configMap: &corev1.ConfigMap{
 				Data: map[string]string{},
 			},
@@ -300,15 +299,25 @@ func TestGenerateCliArgsFromConfigMap(t *testing.T) {
 				"server",
 			},
 		},
+		{
+			name:          "Multiple SubCommands",
+			cliSubCommand: []string{"node-agent", "server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"key": "value",
+				},
+			},
+			expectedArgs: []string{
+				"node-agent",
+				"server",
+				"--key=value",
+			},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotArgs := GenerateCliArgsFromConfigMap(tt.cliSubCommand, tt.configMap)
-			// We don't require order of the args, so the test needs to sort them
-			// before comparison
-			sort.Strings(gotArgs)
-			sort.Strings(tt.expectedArgs)
+			gotArgs := GenerateCliArgsFromConfigMap(tt.configMap, tt.cliSubCommand...)
 			if !reflect.DeepEqual(gotArgs, tt.expectedArgs) {
 				t.Errorf("GenerateCliArgsFromConfigMap() = %v, want %v", gotArgs, tt.expectedArgs)
 			}
