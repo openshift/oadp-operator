eb0d00f2776b073da5989ee4a58c78741d67493c
Michal Pryc <mpryc@redhat.com>
2024-06-28 16:01:06 +0200

    Implementation function for unsupported args. (#1423)
    * Implementation function for unsupported args.
    
    Draft implementation to calculate args from the provided
    configmap with test.
    
    Related #1400
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>
    
    * Reworked implementation for the unsupported args
    
     - Ensure the value is left as is if surrounded by ' or "
     - Include ' if the non-boolean value is missing the single quotes
     - Ensure boolean value is changed to small letters and applied to key=value
     - Ensure value is a string even if it seems to be boolean, when the value
       is surrounded by ' or "
     - Ensure value with prefix - or -- is kept as is
     - Allow key to be passed with or without "--" prefix and resulting schema is as
       cli subcommand --key='value' --another-key='value' --bool-value=true
    
     Updated tests to cover all of the above as well empty ConfigMap
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>
    
    * Unsupported arguments implementation without single quote
    
    Args should not be wrapped with single quotes.
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>
    
    * Unsupported args function to include multiple subCommands
    
    Addition to allow multiple sub commands to be used that
    is required for the node-agent that runs as:
    
     $ velero node-agent server [args]
    
    This change also modifies the args to be alphabetially
    sorted, so the result from the function is consistent across
    it's runs.
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>
    
    * Adjust to address small nits in the design and implementation
    
    Address two nits:
     - numbering inside design doc
     - removal of single quote from the design doc
     - removal of one docstring from the function
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>
    
    * Update to the unsupported args implementation
    
    Additional update to include:
     - single char arg names to be prefixed by default with
       single '-'. It is possible to override this behavior by
       passing '--' as the arg name in the ConfigMap, but the
       default is closer to what user expects.
    
     - Removal of copy() for cliSubCommand
    
     - Small nits
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>
    
    * Do not assume single char value name must be prefixed with -
    
    Remove implementation assumption in which single char
    arg name was prefixed with single dash `-`
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>
    
    ---------
    
    Signed-off-by: Michal Pryc <mpryc@redhat.com>

--- pkg/common/common_test.go
+++ pkg/common/common_test.go
@@ -148,3 +148,133 @@ func TestStripDefaultPorts(t *testing.T) {
 		})
 	}
 }
+
+func TestGenerateCliArgsFromConfigMap(t *testing.T) {
+	tests := []struct {
+		name          string
+		cliSubCommand []string
+		configMap     *corev1.ConfigMap
+		expectedArgs  []string
+	}{
+		{
+			name:          "Boolean argument variations",
+			cliSubCommand: []string{"server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"--default-snapshot-move":   "true",
+					"--another-key-mix-letters": "TrUe",
+					"key-no-prefix":             "False",
+					"string-not-bool":           "'False'",
+				},
+			},
+			expectedArgs: []string{
+				"server",
+				"--another-key-mix-letters=true",
+				"--default-snapshot-move=true",
+				"--key-no-prefix=false",
+				"--string-not-bool='False'",
+			},
+		},
+		{
+			name:          "All arguments with spaces, some without single quotes",
+			cliSubCommand: []string{"server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"default-volume-snapshot-locations": "aws:backups-primary, azure:backups-secondary",
+					"log-level":                         "'debug'",
+				},
+			},
+			expectedArgs: []string{
+				"server",
+				"--default-volume-snapshot-locations=aws:backups-primary, azure:backups-secondary",
+				"--log-level='debug'",
+			},
+		},
+		{
+			name:          "Preserve single and double '-' as key prefix",
+			cliSubCommand: []string{"server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"--default-volume-snapshot-locations": "aws:backups-primary, azure:backups-secondary",
+					"--log-level":                         "debug",
+					"--string-bool":                       "False",
+					"-n":                                  "mynamespace",
+				},
+			},
+			expectedArgs: []string{
+				"server",
+				"--default-volume-snapshot-locations=aws:backups-primary, azure:backups-secondary",
+				"--log-level=debug",
+				"--string-bool=false",
+				"-n=mynamespace",
+			},
+		},
+		{
+			name:          "Non-Boolean argument with space",
+			cliSubCommand: []string{"server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"default-snapshot-move-data": " true",
+				},
+			},
+			expectedArgs: []string{
+				"server",
+				"--default-snapshot-move-data= true",
+			},
+		},
+		{
+			name:          "Mixed arguments",
+			cliSubCommand: []string{"server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"--default-volume-snapshot-locations": "aws:backups-primary,azure:backups-secondary",
+					"--log-level":                         "debug",
+					"--default-snapshot-move-data":        "True",
+					"-v":                                  "3",
+					"a":                                   "somearg",
+				},
+			},
+			expectedArgs: []string{
+				"server",
+				"--a=somearg",
+				"--default-snapshot-move-data=true",
+				"--default-volume-snapshot-locations=aws:backups-primary,azure:backups-secondary",
+				"--log-level=debug",
+				"-v=3",
+			},
+		},
+		{
+			name:          "Empty ConfigMap",
+			cliSubCommand: []string{"server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{},
+			},
+			expectedArgs: []string{
+				"server",
+			},
+		},
+		{
+			name:          "Multiple SubCommands",
+			cliSubCommand: []string{"node-agent", "server"},
+			configMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"key": "value",
+				},
+			},
+			expectedArgs: []string{
+				"node-agent",
+				"server",
+				"--key=value",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotArgs := GenerateCliArgsFromConfigMap(tt.configMap, tt.cliSubCommand...)
+			if !reflect.DeepEqual(gotArgs, tt.expectedArgs) {
+				t.Errorf("GenerateCliArgsFromConfigMap() = %v, want %v", gotArgs, tt.expectedArgs)
+			}
+		})
+	}
+}
