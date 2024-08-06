e475659624ca84be1bcfa45215a2f7d9c15c360c
Matthew Arnold <marnold@redhat.com>
2024-08-05 21:20:16 -0400

    Use UninstallApplication instead of delete.
    Signed-off-by: Matthew Arnold <marnold@redhat.com>

--- tests/e2e/virt_backup_restore_suite_test.go
+++ tests/e2e/virt_backup_restore_suite_test.go
@@ -1,7 +1,7 @@
 package e2e_test
 
 import (
-	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -11,7 +11,6 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -181,7 +180,8 @@ var _ = Describe("VM backup and restore tests", Ordered, func() {
 			v.EnsureVirtRemoval()
 		}
 
-		err := v.Clientset.StorageV1().StorageClasses().Delete(context.TODO(), "test-gp3-csi-immediate", metav1.DeleteOptions{})
+		storageClassYaml := fmt.Sprintf("./sample-applications/storageclass-immediate/%s-immediate.yaml", provider)
+		err := lib.UninstallApplication(v.Client, storageClassYaml)
 		Expect(err).To(BeNil())
 	})
 
