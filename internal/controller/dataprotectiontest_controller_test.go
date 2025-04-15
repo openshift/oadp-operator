ceab679129261cd5cf2ad50838c20ef2044f331f
Shubham Pampattiwar <spampatt@redhat.com>
2025-04-15 13:21:56 -0700

    Add s3 vendor determination tests

--- internal/controller/dataprotectiontest_controller_test.go
+++ internal/controller/dataprotectiontest_controller_test.go
@@ -18,6 +18,11 @@ package controller
 
 import (
 	"context"
+	"github.com/stretchr/testify/require"
+	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -82,3 +87,55 @@ var _ = Describe("DataProtectionTest Controller", func() {
 		})
 	})
 })
+
+func TestDetermineVendor(t *testing.T) {
+	tests := []struct {
+		name           string
+		serverHeader   string
+		expectedVendor string
+	}{
+		{
+			name:           "Detect AWS",
+			serverHeader:   "AmazonS3",
+			expectedVendor: "AWS",
+		},
+		{
+			name:           "Detect MinIO",
+			serverHeader:   "MinIO",
+			expectedVendor: "MinIO",
+		},
+		{
+			name:           "Detect Ceph",
+			serverHeader:   "Ceph",
+			expectedVendor: "Ceph",
+		},
+		{
+			name:           "Unknown vendor",
+			serverHeader:   "SomethingElse",
+			expectedVendor: "somethingelse",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+
+			testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Server", tc.serverHeader)
+			}))
+			defer testServer.Close()
+
+			dpt := &oadpv1alpha1.DataProtectionTest{}
+			dpt.Spec.BackupLocationSpec = &velerov1.BackupStorageLocationSpec{
+				Config: map[string]string{
+					"s3Url": testServer.URL,
+				},
+			}
+
+			reconciler := &DataProtectionTestReconciler{}
+
+			err := reconciler.determineVendor(context.Background(), dpt)
+			require.NoError(t, err)
+			require.Equal(t, tc.expectedVendor, dpt.Status.S3Vendor)
+		})
+	}
+}
