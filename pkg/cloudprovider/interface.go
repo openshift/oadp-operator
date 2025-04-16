36162418276bb5e5adc192d692d959d18876bb6e
Shubham Pampattiwar <spampatt@redhat.com>
2025-04-16 00:01:07 -0700

    Add cloud provider interface

---
+++ pkg/cloudprovider/interface.go
@@ -0,0 +1,17 @@
+package cloudprovider
+
+import (
+	"context"
+	"time"
+
+	oadpv1alpha1 "github.com/openshift/oadp-operator/api/v1alpha1"
+)
+
+// CloudProvider defines operations supported by each cloud.
+type CloudProvider interface {
+	// UploadTest performs a test upload and returns calculated speed and test duration
+	UploadTest(ctx context.Context, config oadpv1alpha1.UploadSpeedTestConfig, bucket string) (int64, time.Duration, error)
+
+	//// GetBucketMetadata retrieves the encryption and versioning config for a bucket
+	//GetBucketMetadata(ctx context.Context, bucket string) (*oadpv1alpha1.BucketMetadata, error)
+}
