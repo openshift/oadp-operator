3ab1dcde69a241252c5d72956aeb5de848a62bb4
Tiger Kaovilai <tkaovila@redhat.com>
2025-05-02 16:40:50 -0400

    WIP: creds testing
    Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

--- pkg/storage/aws/s3_test.go
+++ pkg/storage/aws/s3_test.go
@@ -14,16 +14,39 @@ func TestGetBucketRegion(t *testing.T) {
 		wantErr bool
 	}{
 		{
+			// This should work anonymously, this bucket is made public with Bucket policy
+			// {
+			// 	"Version": "2012-10-17",
+			// 	"Statement": [
+			// 		{
+			// 			"Sid": "publicList",
+			// 			"Effect": "Allow",
+			// 			"Principal": "*",
+			// 			"Action": "s3:ListBucket",
+			// 			"Resource": "arn:aws:s3:::openshift-velero-plugin-s3-auto-region-test-1"
+			// 		}
+			// 	]
+			// }
+			// ❯ aws s3api head-bucket --bucket openshift-velero-plugin-s3-auto-region-test-1 --no-sign-request 
+			// {
+			//     "BucketRegion": "us-east-1",
+			//     "AccessPointAlias": false
+			// }
 			name:    "openshift-velero-plugin-s3-auto-region-test-1",
 			bucket:  "openshift-velero-plugin-s3-auto-region-test-1",
 			region:  "us-east-1",
 			wantErr: false,
 		},
 		{
+			// This should require creds
+			// ❯ aws s3api head-bucket --bucket openshift-velero-plugin-s3-auto-region-test-2 --no-sign-request
+
+			// An error occurred (403) when calling the HeadBucket operation: Forbidden 
 			name:    "openshift-velero-plugin-s3-auto-region-test-2",
 			bucket:  "openshift-velero-plugin-s3-auto-region-test-2",
 			region:  "us-west-1",
 			wantErr: false,
+			// TODO: path/param to creds on ci
 		},
 		{
 			name:    "openshift-velero-plugin-s3-auto-region-test-3",
@@ -42,6 +65,7 @@ func TestGetBucketRegion(t *testing.T) {
 			bucket:  "velero-6109f5e9711c8c58131acdd2f490f451",
 			region:  "us-east-1",
 			wantErr: false,
+			// TODO: add creds usage here.
 		},
 	}
 	for _, tt := range tests {
