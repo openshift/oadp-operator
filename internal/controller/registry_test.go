openshift/oadp-operator

commit 0ff661cae03822fc278e9715af5515c7e89f680d

OADP-5179: Validate secret content in validator for parsing errors (#1672)
* Validate secret content in validator for parsing errors

 to block bsl creation if secret parsing would fail in later steps.

Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

* refactor

Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

* address comments

Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

---------

Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

internal/controller/registry.go
@@ -254,6 +254,7 @@ func (r *DataProtectionApplicationReconciler) getSecretNameAndKeyFromCloudStorag
 	return "", "", nil
 }
 
+// TODO: refactor and reduce duplicate secret gets in registry.go and bsl.go
 func (r *DataProtectionApplicationReconciler) getSecretNameAndKey(config map[string]string, credential *corev1.SecretKeySelector, plugin oadpv1alpha1.DefaultPlugin) (string, string, error) {
 	// Assume default values unless user has overriden them
 	secretName := credentials.PluginSpecificFields[plugin].SecretName
@@ -269,9 +270,13 @@ func (r *DataProtectionApplicationReconciler) getSecretNameAndKey(config map[str
 	if credential != nil {
 		if len(credential.Name) > 0 {
 			secretName = credential.Name
+		} else {
+			return "", "", fmt.Errorf("secret name specified for location cannot be empty")
 		}
 		if len(credential.Key) > 0 {
 			secretKey = credential.Key
+		} else {
+			return "", "", fmt.Errorf("secret key specified for location cannot be empty")
 		}
 	}
 
@@ -777,7 +782,7 @@ func (r *DataProtectionApplicationReconciler) verifySecretContent(secretName str
 	}
 	data, foundKey := secret.Data[secretKey]
 	if !foundKey || len(data) == 0 {
-		return fmt.Errorf("Secret name %s is missing data for key %s", secretName, secretKey)
+		return fmt.Errorf("secret name %s is missing data for key %s", secretName, secretKey)
 	}
 	return nil
 }

internal/controller/bsl.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"slices"
+	"strings"
 
 	"github.com/go-logr/logr"
 	velerov1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
@@ -24,7 +25,6 @@ func (r *DataProtectionApplicationReconciler) ValidateBackupStorageLocations() (
 	dpa := r.dpa
 	numDefaultLocations := 0
 	for _, bslSpec := range dpa.Spec.BackupLocations {
-
 		if err := r.ensureBackupLocationHasVeleroOrCloudStorage(&bslSpec); err != nil {
 			return false, err
 		}
@@ -36,7 +36,6 @@ func (r *DataProtectionApplicationReconciler) ValidateBackupStorageLocations() (
 		if err := r.ensureSecretDataExists(&bslSpec); err != nil {
 			return false, err
 		}
-
 		if bslSpec.Velero != nil {
 			if bslSpec.Velero.Default {
 				numDefaultLocations++
@@ -70,21 +69,11 @@ func (r *DataProtectionApplicationReconciler) ValidateBackupStorageLocations() (
 			}
 		}
 		if bslSpec.CloudStorage != nil {
-			// Make sure credentials are specified.
-			if bslSpec.CloudStorage.Credential == nil {
-				return false, fmt.Errorf("must provide a valid credential secret")
-			}
-			if bslSpec.CloudStorage.Credential.LocalObjectReference.Name == "" {
-				return false, fmt.Errorf("must provide a valid credential secret name")
-			}
 			if bslSpec.CloudStorage.Default {
 				numDefaultLocations++
 			} else if bslSpec.Name == "default" {
 				return false, fmt.Errorf("Storage location named 'default' must be set as default")
 			}
-		}
-		if bslSpec.CloudStorage != nil && bslSpec.Velero != nil {
-			return false, fmt.Errorf("must choose one of bucket or velero")
 		}
 	}
 	if numDefaultLocations > 1 {
@@ -426,36 +415,90 @@ func (r *DataProtectionApplicationReconciler) ensurePrefixWhenBackupImages(bsl *
 }
 
 func (r *DataProtectionApplicationReconciler) ensureSecretDataExists(bsl *oadpv1alpha1.BackupLocation) error {
-	// Check if the Velero feature flag 'no-secret' is not set
-	if !(r.dpa.Spec.Configuration.Velero.HasFeatureFlag("no-secret")) {
-		// Check if the user specified credential under velero
-		if bsl.Velero != nil && bsl.Velero.Credential != nil {
-			// Check if user specified empty credential key
-			if bsl.Velero.Credential.Key == "" {
-				return fmt.Errorf("Secret key specified in BackupLocation %s cannot be empty", bsl.Name)
-			}
-			// Check if user specified empty credential name
-			if bsl.Velero.Credential.Name == "" {
-				return fmt.Errorf("Secret name specified in BackupLocation %s cannot be empty", bsl.Name)
-			}
-		}
-
-		// Check if the BSL secret key configured in the DPA exists with a secret data
-
-		if bsl.CloudStorage != nil {
-			_, _, err := r.getSecretNameAndKeyFromCloudStorage(bsl.CloudStorage)
-			if err != nil {
-				return err
-			}
-		}
-
-		if bsl.Velero != nil {
-			_, _, err := r.getSecretNameAndKey(bsl.Velero.Config, bsl.Velero.Credential, oadpv1alpha1.DefaultPlugin(bsl.Velero.Provider))
-			if err != nil {
-				return err
-			}
-		}
-
+	// Don't check if the Velero feature flag 'no-secret' is set
+	if r.dpa.Spec.Configuration.Velero.HasFeatureFlag("no-secret") {
+		return nil
 	}
+
+	// Extract secret name, key, provider and profile outside the if blocks
+	var secretName, secretKey, provider string
+	var err error
+	awsProfile := "default"
+
+	// Get secret details from either CloudStorage or Velero
+	if bsl.CloudStorage != nil {
+		// Make sure credentials are specified.
+		if bsl.CloudStorage.Credential == nil {
+			return fmt.Errorf("must provide a valid credential secret")
+		}
+		if bsl.CloudStorage.Credential.Name == "" {
+			return fmt.Errorf("must provide a valid credential secret name")
+		}
+		// Check if user specified empty credential key
+		if bsl.CloudStorage.Credential.Key == "" {
+			return fmt.Errorf("must provide a valid credential secret key")
+		}
+		secretName, secretKey, err = r.getSecretNameAndKeyFromCloudStorage(bsl.CloudStorage)
+		if err != nil {
+			return err
+		}
+
+		// Get provider type from CloudStorage
+		if bsl.CloudStorage.CloudStorageRef.Name != "" {
+			bucket := &oadpv1alpha1.CloudStorage{}
+			err := r.Get(r.Context, client.ObjectKey{Namespace: r.dpa.Namespace, Name: bsl.CloudStorage.CloudStorageRef.Name}, bucket)
+			if err != nil {
+				return err
+			} else {
+				provider = string(bucket.Spec.Provider)
+			}
+		}
+
+		// Get AWS profile if specified
+		if bsl.CloudStorage.Config != nil {
+			if value, exists := bsl.CloudStorage.Config[Profile]; exists {
+				awsProfile = value
+			}
+		}
+	} else if bsl.Velero != nil {
+		secretName, secretKey, err = r.getSecretNameAndKey(bsl.Velero.Config, bsl.Velero.Credential, oadpv1alpha1.DefaultPlugin(bsl.Velero.Provider))
+		if err != nil {
+			return err
+		}
+
+		// Get provider type from Velero
+		provider = bsl.Velero.Provider
+
+		// Get AWS profile if specified
+		if bsl.Velero.Config != nil {
+			if value, exists := bsl.Velero.Config[Profile]; exists {
+				awsProfile = value
+			}
+		}
+	}
+
+	// Get the secret, this also ensure secret referenced exists.
+	secret, err := r.getProviderSecret(secretName)
+	if err != nil {
+		return err
+	}
+	// Only parse secrets when backupImages is true
+	if !r.dpa.BackupImages() {
+		return nil
+	}
+	// Parse the secret based on provider type
+	switch {
+	case provider == AWSProvider || strings.Contains(provider, "aws"):
+		_, _, err = r.parseAWSSecret(secret, secretKey, awsProfile)
+		if err != nil {
+			return fmt.Errorf("error parsing AWS secret %s: %v", secretName, err)
+		}
+	case provider == AzureProvider || strings.Contains(provider, "azure"):
+		_, err = r.parseAzureSecret(secret, secretKey)
+		if err != nil {
+			return fmt.Errorf("error parsing Azure secret %s: %v", secretName, err)
+		}
+	}
+
 	return nil
 }

internal/controller/bsl_test.go
@@ -60,6 +60,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 		name    string
 		dpa     *oadpv1alpha1.DataProtectionApplication
 		secret  *corev1.Secret
+		objects []client.Object
 		want    bool
 		wantErr bool
 	}{
@@ -203,7 +204,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 		},
 		{
@@ -405,7 +406,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 		},
 		{
@@ -451,7 +452,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 		},
 		{
@@ -616,7 +617,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 		},
 		{
@@ -868,7 +869,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 		},
 		{
@@ -1124,6 +1125,11 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					},
 				},
 			},
+			objects: []client.Object{
+				&oadpv1alpha1.CloudStorage{
+					ObjectMeta: metav1.ObjectMeta{Name: "testing", Namespace: "test-ns"},
+				},
+			},
 			want:    true,
 			wantErr: false,
 			secret: &corev1.Secret{
@@ -1131,7 +1137,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 		},
 		{
@@ -1207,7 +1213,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 			want:    true,
 			wantErr: false,
@@ -1363,7 +1369,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 			want:    true,
 			wantErr: false,
@@ -1444,7 +1450,7 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 					Name:      "cloud-credentials",
 					Namespace: "test-ns",
 				},
-				Data: map[string][]byte{"cloud": []byte("dummy_data")},
+				Data: map[string][]byte{"cloud": []byte("[default]\naws_access_key_id=AKIAIOSFODNN7EXAMPLE\naws_secret_access_key=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")},
 			},
 			want:    true,
 			wantErr: false,
@@ -1490,7 +1496,8 @@ func TestDPAReconciler_ValidateBackupStorageLocations(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fakeClient, err := getFakeClientFromObjects(tt.dpa, tt.secret)
+			tt.objects = append(tt.objects, tt.dpa, tt.secret)
+			fakeClient, err := getFakeClientFromObjects(tt.objects...)
 			if err != nil {
 				t.Errorf("error in creating fake client, likely programmer error")
 			}

commit c44e641c43c8d83088f316a0dff74f4e9aba9d9e

OADP-6105: Handle STS Cred in parseAWSSecret
regression from https://github.com/openshift/oadp-operator/pull/1672 for OADP-5179

Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

internal/controller/registry.go
@@ -296,6 +296,10 @@ func (r *DataProtectionApplicationReconciler) parseAWSSecret(secret corev1.Secre
 	const (
 		accessKeyKey = "aws_access_key_id"
 		secretKeyKey = "aws_secret_access_key"
+		// STS specific fields
+		roleArnKey              = "role_arn"
+		webIdentityTokenFileKey = "web_identity_token_file"
+		stsRegionalEndpointsKey = "sts_regional_endpoints"
 	)
 	if err != nil {
 		return AWSAccessKey, AWSSecretKey, errors.New("parseAWSSecret faulty regex: keyNameRegex")
@@ -307,6 +311,19 @@ func (r *DataProtectionApplicationReconciler) parseAWSSecret(secret corev1.Secre
 	awsSecretKeyRegex, err := regexp.Compile(`\b` + secretKeyKey + `\b`)
 	if err != nil {
 		return AWSAccessKey, AWSSecretKey, errors.New("parseAWSSecret faulty regex: awsSecretKeyRegex")
+	}
+	// Compile regex for STS fields
+	roleArnRegex, err := regexp.Compile(`\b` + roleArnKey + `\b`)
+	if err != nil {
+		return AWSAccessKey, AWSSecretKey, errors.New("parseAWSSecret faulty regex: roleArnRegex")
+	}
+	webIdentityTokenFileRegex, err := regexp.Compile(`\b` + webIdentityTokenFileKey + `\b`)
+	if err != nil {
+		return AWSAccessKey, AWSSecretKey, errors.New("parseAWSSecret faulty regex: webIdentityTokenFileRegex")
+	}
+	stsRegionalEndpointsRegex, err := regexp.Compile(`\b` + stsRegionalEndpointsKey + `\b`)
+	if err != nil {
+		return AWSAccessKey, AWSSecretKey, errors.New("parseAWSSecret faulty regex: stsRegionalEndpointsRegex")
 	}
 	for index, line := range splitString {
 		if line == "" {
@@ -325,42 +342,78 @@ func (r *DataProtectionApplicationReconciler) parseAWSSecret(secret corev1.Secre
 				if index+1 >= len(splitString) {
 					break
 				}
+
+				// Check if this profile contains STS fields
+				hasStsFields := false
 				for _, profLine := range splitString[index+1:] {
 					if profLine == "" {
 						continue
 					}
-					matchedAccessKey := awsAccessKeyRegex.MatchString(profLine)
-					matchedSecretKey := awsSecretKeyRegex.MatchString(profLine)
-
-					if err != nil {
-						r.Log.Info("Error finding access key id for the supplied AWS credential")
-						return AWSAccessKey, AWSSecretKey, err
+					if keyNameRegex.MatchString(profLine) {
+						// We've reached the next profile
+						break
 					}
-					if matchedAccessKey { // check for access key
-						AWSAccessKey, err = r.getMatchedKeyValue(accessKeyKey, profLine)
-						if err != nil {
-							r.Log.Info("Error processing access key id for the supplied AWS credential")
-							return AWSAccessKey, AWSSecretKey, err
+
+					// Check for STS-specific fields
+					if roleArnRegex.MatchString(profLine) ||
+						webIdentityTokenFileRegex.MatchString(profLine) ||
+						stsRegionalEndpointsRegex.MatchString(profLine) {
+						hasStsFields = true
+						r.Log.Info(fmt.Sprintf("Detected STS authentication in profile %s", matchProfile))
+						// For STS profiles, we return empty strings for access key and secret key
+						// but don't error out
+						return "", "", nil
+					}
+				}
+
+				// If not an STS profile, continue with normal AWS credential parsing
+				if !hasStsFields {
+					for _, profLine := range splitString[index+1:] {
+						if profLine == "" {
+							continue
 						}
-						continue
-					} else if matchedSecretKey { // check for secret key
-						AWSSecretKey, err = r.getMatchedKeyValue(secretKeyKey, profLine)
-						if err != nil {
-							r.Log.Info("Error processing secret key id for the supplied AWS credential")
-							return AWSAccessKey, AWSSecretKey, err
+						if keyNameRegex.MatchString(profLine) {
+							// We've reached the next profile
+							break
 						}
-						continue
-					} else {
-						break // aws credentials file is only allowed to have profile followed by aws_access_key_id, aws_secret_access_key
+
+						matchedAccessKey := awsAccessKeyRegex.MatchString(profLine)
+						matchedSecretKey := awsSecretKeyRegex.MatchString(profLine)
+
+						if matchedAccessKey { // check for access key
+							AWSAccessKey, err = r.getMatchedKeyValue(accessKeyKey, profLine)
+							if err != nil {
+								r.Log.Info("Error processing access key id for the supplied AWS credential")
+								return AWSAccessKey, AWSSecretKey, err
+							}
+							continue
+						} else if matchedSecretKey { // check for secret key
+							AWSSecretKey, err = r.getMatchedKeyValue(secretKeyKey, profLine)
+							if err != nil {
+								r.Log.Info("Error processing secret key id for the supplied AWS credential")
+								return AWSAccessKey, AWSSecretKey, err
+							}
+							continue
+						}
 					}
 				}
 			}
 		}
 	}
+
 	if profile == "" {
 		r.Log.Info("Error finding AWS Profile for the supplied AWS credential")
 		return AWSAccessKey, AWSSecretKey, errors.New("error finding AWS Profile for the supplied AWS credential")
 	}
+
+	// If we get here and both access key and secret key are empty, it means we found the profile
+	// but it didn't have the expected credentials or STS fields
+	if AWSAccessKey == "" && AWSSecretKey == "" {
+		r.Log.Info(fmt.Sprintf("Profile %s found but no credentials or STS fields detected", matchProfile))
+		return AWSAccessKey, AWSSecretKey, errors.New("no credentials or STS fields found in profile " + matchProfile)
+	}
+
+	// For regular AWS credentials, both access key and secret key must be present
 	if AWSAccessKey == "" {
 		r.Log.Info("Error finding access key id for the supplied AWS credential")
 		return AWSAccessKey, AWSSecretKey, errors.New("error finding access key id for the supplied AWS credential")

internal/controller/registry_test.go
@@ -58,7 +58,11 @@ var (
 				"aws_secret_access_key=" + testSecretAccessKey +
 				"\n[test-profile]\n" +
 				"aws_access_key_id=" + testAccessKey + "\n" +
-				"aws_secret_access_key=" + testSecretAccessKey,
+				"aws_secret_access_key=" + testSecretAccessKey +
+				"\n[sts]" + "\n" +
+				"sts_regional_endpoints=" + "regional" + "\n" +
+				"role_arn=" + "arn:aws:iam::123456:role/oadp-sample-role" + "\n" +
+				"web_identity_token_file=" + "/var/run/secrets/openshift/serviceaccount/token" + "\n",
 		),
 	}
 	secretDataWithEqualInSecret = map[string][]byte{
@@ -513,6 +517,274 @@ func TestDPAReconciler_populateAzureRegistrySecret(t *testing.T) {
 			}
 			if !reflect.DeepEqual(tt.registrySecret.Data, wantRegistrySecret.Data) {
 				t.Errorf("expected azure registry secret to be %#v, got %#v", tt.registrySecret, wantRegistrySecret.Data)
+			}
+		})
+	}
+}
+
+func TestDPAReconciler_parseAWSSecret(t *testing.T) {
+	tests := []struct {
+		name          string
+		secret        corev1.Secret
+		secretKey     string
+		matchProfile  string
+		wantAccessKey string
+		wantSecretKey string
+		wantErr       bool
+	}{
+		{
+			name: "successful parse with bslProfile",
+			secret: corev1.Secret{
+				Data: secretData,
+			},
+			secretKey:     "cloud",
+			matchProfile:  testBslProfile,
+			wantAccessKey: testBslAccessKey,
+			wantSecretKey: testBslSecretAccessKey,
+			wantErr:       false,
+		},
+		{
+			name: "successful parse with default profile",
+			secret: corev1.Secret{
+				Data: secretData,
+			},
+			secretKey:     "cloud",
+			matchProfile:  "default",
+			wantAccessKey: testAccessKey,
+			wantSecretKey: testSecretAccessKey,
+			wantErr:       false,
+		},
+		{
+			name: "successful parse with test-profile",
+			secret: corev1.Secret{
+				Data: secretData,
+			},
+			secretKey:     "cloud",
+			matchProfile:  "test-profile",
+			wantAccessKey: testAccessKey,
+			wantSecretKey: testSecretAccessKey,
+			wantErr:       false,
+		},
+		{
+			name: "error with missing profile",
+			secret: corev1.Secret{
+				Data: secretData,
+			},
+			secretKey:    "cloud",
+			matchProfile: "non-existent-profile",
+			wantErr:      true,
+		},
+		{
+			name: "successful parse with equals in secret key",
+			secret: corev1.Secret{
+				Data: secretDataWithEqualInSecret,
+			},
+			secretKey:     "cloud",
+			matchProfile:  testBslProfile,
+			wantAccessKey: testBslAccessKey,
+			wantSecretKey: testBslSecretAccessKey + "=" + testBslSecretAccessKey,
+			wantErr:       false,
+		},
+		{
+			name: "successful parse with carriage returns",
+			secret: corev1.Secret{
+				// We need to replace carriage returns as this would happen in getProviderSecret
+				Data: replaceCarriageReturn(secretDataWithCarriageReturnInSecret, logr.Discard()),
+			},
+			secretKey:     "cloud",
+			matchProfile:  testBslProfile,
+			wantAccessKey: testBslAccessKey,
+			wantSecretKey: testBslSecretAccessKey + "=" + testBslSecretAccessKey,
+			wantErr:       false,
+		},
+		{
+			name: "successful parse with mixed quotes and spaces",
+			secret: corev1.Secret{
+				Data: secretDataWithMixedQuotesAndSpacesInSecret,
+			},
+			secretKey:     "cloud",
+			matchProfile:  testBslProfile,
+			wantAccessKey: testBslAccessKey,
+			wantSecretKey: testBslSecretAccessKey,
+			wantErr:       false,
+		},
+		{
+			name: "error with missing profile in secret",
+			secret: corev1.Secret{
+				Data: awsSecretDataWithMissingProfile,
+			},
+			secretKey:    "cloud",
+			matchProfile: testBslProfile,
+			wantErr:      true,
+		},
+		{
+			name: "successful parse with STS profile",
+			secret: corev1.Secret{
+				Data: secretData,
+			},
+			secretKey:     "cloud",
+			matchProfile:  "sts",
+			wantAccessKey: "",
+			wantSecretKey: "",
+			wantErr:       false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DataProtectionApplicationReconciler{
+				Log: logr.Discard(),
+			}
+			gotAccessKey, gotSecretKey, err := r.parseAWSSecret(tt.secret, tt.secretKey, tt.matchProfile)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseAWSSecret() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				if gotAccessKey != tt.wantAccessKey {
+					t.Errorf("parseAWSSecret() gotAccessKey = %v, want %v", gotAccessKey, tt.wantAccessKey)
+				}
+				if gotSecretKey != tt.wantSecretKey {
+					t.Errorf("parseAWSSecret() gotSecretKey = %v, want %v", gotSecretKey, tt.wantSecretKey)
+				}
+			}
+		})
+	}
+}
+
+func TestDPAReconciler_parseAzureSecret(t *testing.T) {
+	tests := []struct {
+		name      string
+		secret    corev1.Secret
+		secretKey string
+		wantCreds azureCredentials
+		wantErr   bool
+	}{
+		{
+			name: "successful parse with storage key only",
+			secret: corev1.Secret{
+				Data: secretAzureData,
+			},
+			secretKey: "cloud",
+			wantCreds: azureCredentials{
+				strorageAccountKey: testStoragekey,
+				subscriptionID:     "",
+				tenantID:           "",
+				clientID:           "",
+				clientSecret:       "",
+				resourceGroup:      "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "successful parse with service principal data",
+			secret: corev1.Secret{
+				Data: secretAzureServicePrincipalData,
+			},
+			secretKey: "cloud",
+			wantCreds: azureCredentials{
+				strorageAccountKey: testStoragekey,
+				subscriptionID:     testSubscriptionID,
+				tenantID:           testTenantID,
+				clientID:           testClientID,
+				clientSecret:       testClientSecret,
+				resourceGroup:      testResourceGroup,
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DataProtectionApplicationReconciler{
+				Log: logr.Discard(),
+			}
+			gotCreds, err := r.parseAzureSecret(tt.secret, tt.secretKey)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseAzureSecret() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !tt.wantErr {
+				if gotCreds.strorageAccountKey != tt.wantCreds.strorageAccountKey {
+					t.Errorf("parseAzureSecret() strorageAccountKey = %v, want %v", gotCreds.strorageAccountKey, tt.wantCreds.strorageAccountKey)
+				}
+				if gotCreds.subscriptionID != tt.wantCreds.subscriptionID {
+					t.Errorf("parseAzureSecret() subscriptionID = %v, want %v", gotCreds.subscriptionID, tt.wantCreds.subscriptionID)
+				}
+				if gotCreds.tenantID != tt.wantCreds.tenantID {
+					t.Errorf("parseAzureSecret() tenantID = %v, want %v", gotCreds.tenantID, tt.wantCreds.tenantID)
+				}
+				if gotCreds.clientID != tt.wantCreds.clientID {
+					t.Errorf("parseAzureSecret() clientID = %v, want %v", gotCreds.clientID, tt.wantCreds.clientID)
+				}
+				if gotCreds.clientSecret != tt.wantCreds.clientSecret {
+					t.Errorf("parseAzureSecret() clientSecret = %v, want %v", gotCreds.clientSecret, tt.wantCreds.clientSecret)
+				}
+				if gotCreds.resourceGroup != tt.wantCreds.resourceGroup {
+					t.Errorf("parseAzureSecret() resourceGroup = %v, want %v", gotCreds.resourceGroup, tt.wantCreds.resourceGroup)
+				}
+			}
+		})
+	}
+}
+
+func TestDPAReconciler_getMatchedKeyValue(t *testing.T) {
+	tests := []struct {
+		name    string
+		key     string
+		s       string
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "successful parse with simple value",
+			key:     "aws_access_key_id",
+			s:       "aws_access_key_id=AKIAIOSFODNN7EXAMPLE",
+			want:    "AKIAIOSFODNN7EXAMPLE",
+			wantErr: false,
+		},
+		{
+			name:    "successful parse with quoted value",
+			key:     "aws_secret_access_key",
+			s:       "aws_secret_access_key=\"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY\"",
+			want:    "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+			wantErr: false,
+		},
+		{
+			name:    "successful parse with single quoted value",
+			key:     "aws_secret_access_key",
+			s:       "aws_secret_access_key='wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'",
+			want:    "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+			wantErr: false,
+		},
+		{
+			name:    "successful parse with spaces",
+			key:     "aws_access_key_id",
+			s:       " aws_access_key_id = AKIAIOSFODNN7EXAMPLE ",
+			want:    "AKIAIOSFODNN7EXAMPLE",
+			wantErr: false,
+		},
+		{
+			name:    "error with empty value",
+			key:     "aws_access_key_id",
+			s:       "aws_access_key_id=",
+			want:    "",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &DataProtectionApplicationReconciler{
+				Log: logr.Discard(),
+			}
+			got, err := r.getMatchedKeyValue(tt.key, tt.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getMatchedKeyValue() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("getMatchedKeyValue() = %v, want %v", got, tt.want)
 			}
 		})
 	}