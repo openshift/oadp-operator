c6ce5d58adfc34fe95953136d042cc6d8792f54e
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2025-06-20 11:44:07 +0200

    NO-JIRA: Update Konflux new images for HCP OADP Plugin
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -75,7 +75,7 @@ const (
 	GCPPluginImage        = "quay.io/konveyor/velero-plugin-for-gcp:latest"
 	RegistryImage         = "quay.io/konveyor/registry:latest"
 	KubeVirtPluginImage   = "quay.io/konveyor/kubevirt-velero-plugin:v0.7.0"
-	HypershiftPluginImage = "quay.io/redhat-user-workloads/crt-redhat-acm-tenant/hypershift-oadp-plugin-main:latest"
+	HypershiftPluginImage = "quay.io/redhat-user-workloads/ocp-art-tenant/oadp-hypershift-oadp-plugin-main:main"
 )
 
 // Plugin names
