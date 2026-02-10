d6c49030c51489da3374445300ef0f0c84a2ecf9
Joseph Antony Vaikath <joeavaikath@gmail.com>
2026-02-10 16:59:22 -0500

    Add ConsoleCLIDownload for oadp-cli (#1997)
    * Add ConsoleCLIDownload for oadp-cli
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Setup oadp-cli binary server on operator startup - First pass, route and ConsoleCLIDownload will be moved into a controller
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Use controller to startup route and ConsoleCLIDownload
    
    Better than piggybacking off leader election, if leader changes it will repeatcontroller can better ensure route changes and deployment changes don't affect the end goal of having ConsoleCLIDownload with the right link
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Fix download link text
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Linting
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Use konveyor org
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Start oadp-cli on server startup
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Make bundle
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Do not initialize controller if watchNamespace is empty
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    * Add bounded retries, exponential backoff and context awareness
    
    Signed-off-by: Joseph <jvaikath@redhat.com>
    
    ---------
    
    Signed-off-by: Joseph <jvaikath@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -27,6 +27,7 @@ import (
 
 	snapshotv1api "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
 	configv1 "github.com/openshift/api/config/v1"
+	consolev1 "github.com/openshift/api/console/v1"
 	routev1 "github.com/openshift/api/route/v1"
 	security "github.com/openshift/api/security/v1"
 	monitor "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
@@ -227,6 +228,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err := consolev1.AddToScheme(mgr.GetScheme()); err != nil {
+		setupLog.Error(err, "unable to add OpenShift console API to scheme")
+		os.Exit(1)
+	}
+
 	if err := velerov1.AddToScheme(mgr.GetScheme()); err != nil {
 		setupLog.Error(err, "unable to add Velero APIs to scheme")
 		os.Exit(1)
@@ -292,6 +298,22 @@ func main() {
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
+
+	// Add CLI download setup runnable
+	// Only add if watchNamespace is set (skip for cluster-scoped mode)
+	if watchNamespace != "" {
+		if err := mgr.Add(&controller.CLIDownloadSetup{
+			Client:            mgr.GetClient(),
+			Namespace:         watchNamespace,
+			OperatorName:      "openshift-adp-controller-manager",
+			OperatorNamespace: watchNamespace,
+		}); err != nil {
+			setupLog.Error(err, "unable to add CLI download setup")
+			os.Exit(1)
+		}
+	} else {
+		setupLog.Info("Skipping CLI download setup - watchNamespace not set")
+	}
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
