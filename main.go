d7e87816cef73bee630c886f66843da0d6c737ad
Nicholas Yancey <yellowvelinia@gmail.com>
2026-07-14 10:46:00 -0400

    Backport cli download controller to oadp-1.3

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	consolev1 "github.com/openshift/api/console/v1"
 	routev1 "github.com/openshift/api/route/v1"
 	security "github.com/openshift/api/security/v1"
 	"github.com/openshift/oadp-operator/controllers"
@@ -199,6 +200,29 @@ func main() {
 	if err := v1.AddToScheme(mgr.GetScheme()); err != nil {
 		setupLog.Error(err, "unable to add Kubernetes API extensions to scheme")
 		os.Exit(1)
+	}
+
+	if err := consolev1.AddToScheme(mgr.GetScheme()); err != nil {
+		setupLog.Error(err, "unable to add OpenShift console API to scheme")
+		os.Exit(1)
+	}
+
+	// Add CLI download setup runnable.
+	// Skip when the ConsoleCLIDownload CRD is absent (clusters without Console capability, e.g. SNO).
+	if available, err := controllers.IsConsoleCRDAvailable(mgr.GetRESTMapper(), setupLog); !available {
+		if err != nil {
+			setupLog.Error(err, "unable to check ConsoleCLIDownload CRD availability, skipping CLI download setup")
+		}
+	} else {
+		if err := mgr.Add(&controllers.CLIDownloadSetup{
+			Client:            mgr.GetClient(),
+			Namespace:         watchNamespace,
+			OperatorName:      "openshift-adp-controller-manager",
+			OperatorNamespace: watchNamespace,
+		}); err != nil {
+			setupLog.Error(err, "unable to add CLI download setup")
+			os.Exit(1)
+		}
 	}
 
 	if err = (&controllers.DPAReconciler{
