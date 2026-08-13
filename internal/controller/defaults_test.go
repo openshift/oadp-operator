d29e2c57bc3cd8af15453b3352a20713aea4516a
MICHAEL FRUCHTMAN <msfrucht@us.ibm.com>
2026-08-13 12:14:58 -0700

    Add test of defaults due to more complex logic returning new object

---
+++ internal/controller/defaults_test.go
@@ -0,0 +1,81 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/vmware-tanzu/velero/pkg/util/kube"
+)
+
+func Test_newPodResourcesWithUnboundedDefaults(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *kube.PodResources
+		want  *kube.PodResources
+	}{
+		{
+			name:  "nil returns nil",
+			input: nil,
+			want:  nil,
+		},
+		{
+			name: "partially set fields get unset values replaced with unbounded",
+			input: &kube.PodResources{
+				CPURequest:    "100m",
+				MemoryRequest: "128Mi",
+			},
+			want: &kube.PodResources{
+				CPURequest:              "100m",
+				CPULimit:                "0",
+				MemoryRequest:           "128Mi",
+				MemoryLimit:             "0",
+				EphemeralStorageRequest: "0",
+				EphemeralStorageLimit:   "0",
+			},
+		},
+		{
+			name: "all fields set returns unchanged",
+			input: &kube.PodResources{
+				CPURequest:              "100m",
+				CPULimit:                "200m",
+				MemoryRequest:           "128Mi",
+				MemoryLimit:             "256Mi",
+				EphemeralStorageRequest: "1Gi",
+				EphemeralStorageLimit:   "2Gi",
+			},
+			want: &kube.PodResources{
+				CPURequest:              "100m",
+				CPULimit:                "200m",
+				MemoryRequest:           "128Mi",
+				MemoryLimit:             "256Mi",
+				EphemeralStorageRequest: "1Gi",
+				EphemeralStorageLimit:   "2Gi",
+			},
+		},
+		{
+			name:  "zero-value struct gets all fields set to unbounded",
+			input: &kube.PodResources{},
+			want: &kube.PodResources{
+				CPURequest:              "0",
+				CPULimit:                "0",
+				MemoryRequest:           "0",
+				MemoryLimit:             "0",
+				EphemeralStorageRequest: "0",
+				EphemeralStorageLimit:   "0",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := newPodResourcesWithUnboundedDefaults(tt.input)
+			require.Equal(t, tt.want, got)
+
+			// require the output object is not a mutation of the existing object
+			if tt.input != nil {
+				require.NotSame(t, tt.input, got)
+			}
+		})
+	}
+}
