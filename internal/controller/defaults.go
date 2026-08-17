6b06adb50329431e9803c49a2174a88636891d63
Michael Fruchtman <msfrucht@us.ibm.com>
2026-08-17 09:42:45 -0700

    revise to handle older and newer versions of PodResources with strings, unit test changes requested
    Signed-off-by: Michael Fruchtman <msfrucht@us.ibm.com>

--- internal/controller/defaults.go
+++ internal/controller/defaults.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"reflect"
+
 	"github.com/vmware-tanzu/velero/pkg/util/kube"
 )
 
@@ -14,44 +16,27 @@ const unbounded = "0"
 // PodResources with emptystring will trigger parsing errors in Velero.
 // Replace empty string with unbounded so partial resource setting is accepted.
 //
-// Returns a new PodResources objects with any default values set to "0".
+// Returns a new PodResources object with any empty string fields set to "0".
 // If nil, returns the existing nil pointer.
 func newPodResourcesWithUnboundedDefaults(pr *kube.PodResources) *kube.PodResources {
 	if pr == nil {
 		return pr
 	}
 
-	prWithUnboundedDefaults := &kube.PodResources{}
-	if pr.CPURequest == "" {
-		prWithUnboundedDefaults.CPURequest = unbounded
-	} else {
-		prWithUnboundedDefaults.CPURequest = pr.CPURequest
+	prWithUnboundedDefaults := *pr
+	// Velero 1.18.1 adds ephemeralStorageRequest and ephemeralStorageLimits not in prior versions.
+	// Reflection handles new versions provided the new underlying fields are strings.
+	// Velero 1.18.1 and below are handled due to all fields are strings.
+	reflectedPr := reflect.ValueOf(pr).Elem()
+	reflectNewPr := reflect.ValueOf(&prWithUnboundedDefaults).Elem()
+	for i := range reflectedPr.NumField() {
+		oldField := reflectedPr.Field(i)
+		newField := reflectNewPr.Field(i)
+		if oldField.Kind() == reflect.String && oldField.String() == "" {
+			newField.SetString(unbounded)
+		} else {
+			newField.Set(oldField)
+		}
 	}
-	if pr.CPULimit == "" {
-		prWithUnboundedDefaults.CPULimit = unbounded
-	} else {
-		prWithUnboundedDefaults.CPULimit = pr.CPULimit
-	}
-	if pr.MemoryRequest == "" {
-		prWithUnboundedDefaults.MemoryRequest = unbounded
-	} else {
-		prWithUnboundedDefaults.MemoryRequest = pr.MemoryRequest
-	}
-	if pr.MemoryLimit == "" {
-		prWithUnboundedDefaults.MemoryLimit = unbounded
-	} else {
-		prWithUnboundedDefaults.MemoryLimit = pr.MemoryLimit
-	}
-	if pr.EphemeralStorageRequest == "" {
-		prWithUnboundedDefaults.EphemeralStorageRequest = unbounded
-	} else {
-		prWithUnboundedDefaults.EphemeralStorageRequest = pr.EphemeralStorageRequest
-	}
-	if pr.EphemeralStorageLimit == "" {
-		prWithUnboundedDefaults.EphemeralStorageLimit = unbounded
-	} else {
-		prWithUnboundedDefaults.EphemeralStorageLimit = pr.EphemeralStorageLimit
-	}
-
-	return prWithUnboundedDefaults
+	return &prWithUnboundedDefaults
 }
